20f0f6fe3bf592fb9de7f9268df9c35ff06eed05
Mikhail Yakshin <greycat@altlinux.org>
2016-02-17 19:27:48 +0300

    Made some vital attributes properly Scala-like

--- src/main/scala/io/kaitai/structures/format/AttrSpec.scala
+++ src/main/scala/io/kaitai/structures/format/AttrSpec.scala
@@ -10,14 +10,19 @@ case class AttrSpec(@JsonProperty("id") id: String,
                     @JsonProperty("process") process: String,
                     @JsonProperty("contents") contents: Object,
                     @JsonProperty("byte_size") byteSize: String,
-                    @JsonProperty("size") size: String,
+                    @JsonProperty("size") _size: String,
                     @JsonProperty("size_eos") sizeEos: Boolean,
-                    @JsonProperty("if") ifExpr: String,
-                    @JsonProperty("encoding") encoding: String,
-                    @JsonProperty("repeat") repeat: String,
-                    @JsonProperty("repeat-expr") repeatExpr: String,
+                    @JsonProperty("if") _ifExpr: String,
+                    @JsonProperty("encoding") _encoding: String,
+                    @JsonProperty("repeat") _repeat: String,
+                    @JsonProperty("repeat-expr") _repeatExpr: String,
                     @JsonProperty("terminator") terminator: String,
                     @JsonProperty("consume") consume: Boolean,
                     @JsonProperty("include") include: Boolean
                   ) {
+  val size = Option(_size)
+  val ifExpr = Option(_ifExpr)
+  val encoding = Option(_encoding)
+  val repeat = Option(_repeat)
+  val repeatExpr = Option(_repeatExpr)
 }
