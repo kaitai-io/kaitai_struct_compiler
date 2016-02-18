5da153fd8a93cc290c3ac0a8f4a90297e99de033
Mikhail Yakshin <greycat@altlinux.org>
2016-02-18 14:45:16 +0300

    Proper typechecking for string attributes in AttrSpec; added some helper methods

---
+++ src/main/scala/io/kaitai/structures/Utils.scala
@@ -0,0 +1,14 @@
+package io.kaitai.structures
+
+object Utils {
+  private val RDecimal = "^(-?[0-9]+)$".r
+  private val RHex = "^0x([0-9a-fA-F]+)$".r
+
+  def strToOptInt(s: String): Option[Int] = {
+    s match {
+      case null => None
+      case RDecimal(_) => Some(s.toInt)
+      case RHex(hex) => Some(Integer.parseInt(hex, 16))
+    }
+  }
+}
--- src/main/scala/io/kaitai/structures/format/AttrSpec.scala
+++ src/main/scala/io/kaitai/structures/format/AttrSpec.scala
@@ -2,6 +2,8 @@ package io.kaitai.structures.format
 
 import java.util.{List => JList, Map => JMap}
 
+import io.kaitai.structures.Utils
+
 import collection.JavaConversions._
 import com.fasterxml.jackson.annotation.JsonProperty
 
@@ -16,9 +18,10 @@ case class AttrSpec(@JsonProperty("id") id: String,
                     @JsonProperty("encoding") _encoding: String,
                     @JsonProperty("repeat") _repeat: String,
                     @JsonProperty("repeat-expr") _repeatExpr: String,
-                    @JsonProperty("terminator") terminator: String,
-                    @JsonProperty("consume") consume: Boolean,
-                    @JsonProperty("include") include: Boolean
+                    @JsonProperty("terminator") _terminator: String,
+                    @JsonProperty("consume") _consume: String,
+                    @JsonProperty("include") _include: String,
+                    @JsonProperty("eos_error") _eosError: String
                   ) {
   val byteSize = Option(_byteSize)
   val size = Option(_size)
@@ -26,6 +29,19 @@ case class AttrSpec(@JsonProperty("id") id: String,
   val encoding = Option(_encoding)
   val repeat = Option(_repeat)
   val repeatExpr = Option(_repeatExpr)
+  val terminator = Utils.strToOptInt(_terminator).getOrElse(0)
+
+  val consume = boolFromStr(_consume, true)
+  val include = boolFromStr(_include, false)
+  val eosError = boolFromStr(_eosError, true)
 
   def isArray: Boolean = repeat.isDefined
+
+  private def boolFromStr(s: String, byDef: Boolean): Boolean = {
+    s match {
+      case "true" | "yes" | "1" => true
+      case "false" | "no" | "0" | "" => false
+      case null => byDef
+    }
+  }
 }
