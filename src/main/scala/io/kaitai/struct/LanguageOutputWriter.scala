ac139b4b2292a89b4f8efc43f3e9286cfd5e4513
Mikhail Yakshin <greycat@altlinux.org>
2016-02-20 17:20:09 +0300

    LanguageOutputWriter: make sure to create all relevant components of output file paths (akin to "mkdir -p")

--- src/main/scala/io/kaitai/struct/LanguageOutputWriter.scala
+++ src/main/scala/io/kaitai/struct/LanguageOutputWriter.scala
@@ -1,9 +1,11 @@
 package io.kaitai.struct
 
-import java.io.{FileOutputStream, OutputStreamWriter, PrintWriter}
+import java.io.{File, FileOutputStream, OutputStreamWriter, PrintWriter}
 import java.nio.charset.StandardCharsets
 
 class LanguageOutputWriter(fileName: String, indentStr: String) {
+  val outDir = new File(fileName).getParentFile()
+  outDir.mkdirs
   val out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8), true)
   var indentLevel = 0
 
