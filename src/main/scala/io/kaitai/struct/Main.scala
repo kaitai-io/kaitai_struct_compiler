c6e56de1c20e22e043540a2e2b96e8a63bae5fb9
Mikhail Yakshin <greycat@altlinux.org>
2016-02-23 22:44:03 +0300

    Try to make useful shortcut for batch compiling

--- src/main/scala/io/kaitai/struct/Main.scala
+++ src/main/scala/io/kaitai/struct/Main.scala
@@ -1,8 +1,16 @@
 package io.kaitai.struct
 
+import java.io.File
+
 import io.kaitai.struct.languages.{PythonCompiler, JavaCompiler, RubyCompiler}
 
 object Main {
+  def allInputFilesInDir(dir: String): List[String] = {
+    new File(dir).listFiles.filter((f) =>
+      f.isFile && f.getName.endsWith(".ksy")
+    ).map(_.toString).toList
+  }
+
   def main(args : Array[String]): Unit = {
     if (args.length != 3) {
       Console.println("Usage: compiler <lang> <input.yaml> <output.lang>")
@@ -10,6 +18,15 @@ object Main {
     }
 
     val langCompiler = args(0) match {
+      case "all" =>
+        val outDir = args(2)
+        allInputFilesInDir(args(1)).foreach((fn) => {
+          val origId = fn.replace(".ksy", "")
+          new ClassCompiler(fn, new JavaCompiler(s"${outDir}/java")).compile
+          new ClassCompiler(fn, new PythonCompiler(s"${outDir}/python/${origId}.py}")).compile
+          new ClassCompiler(fn, new RubyCompiler(s"${outDir}/ruby/${origId}.rb}")).compile
+        })
+        return
       case "java" => new JavaCompiler(args(2))
       case "python" => new PythonCompiler(args(2))
       case "ruby" => new RubyCompiler(args(2))
