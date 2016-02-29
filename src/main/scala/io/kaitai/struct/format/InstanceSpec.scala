9d46a3e12b857c66d7b698a5ed303d21207427ad
Mikhail Yakshin <greycat@altlinux.org>
2016-03-01 01:47:13 +0300

    Added support for value field in InstanceSpec

--- src/main/scala/io/kaitai/struct/format/InstanceSpec.scala
+++ src/main/scala/io/kaitai/struct/format/InstanceSpec.scala
@@ -24,7 +24,27 @@ class InstanceSpec(
   @JsonProperty("include") _include: String,
   @JsonProperty("eos_error") _eosError: String,
 
-  @JsonProperty("position_abs") _positionAbs: String
+  @JsonProperty("position_abs") _positionAbs: String,
+  @JsonProperty("value") _value: String
 ) extends AttrSpec(id, dataType, process, contents, _byteSize, _size, sizeEos, _ifExpr, _encoding, _repeat, _repeatExpr, _terminator, _consume, _include, _eosError) {
   val positionAbs = Option(_positionAbs).map(Expressions.parse)
+  val value = Option(_value).map(e =>
+    if (dataType != null) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'type'")
+    } else if (process != null) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'process'")
+    } else if (contents != null) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'contents'")
+    } else if (_size != null) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'size'")
+    } else if (_repeat != null) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'repeat'")
+    } else if (_repeatExpr != null) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'repeat-expr'")
+    } else if (positionAbs.isDefined) {
+      throw new RuntimeException(s"instance '${id}': can't specify both 'value' and 'position_abs'")
+    } else {
+      Expressions.parse(e)
+    }
+  )
 }
