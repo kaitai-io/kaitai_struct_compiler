507f0c04666ab5e630c4fc49326589011170351a
Mikhail Yakshin <greycat@altlinux.org>
2016-03-02 15:30:09 +0300

    Added UserType

--- src/main/scala/io/kaitai/struct/exprlang/DataType.scala
+++ src/main/scala/io/kaitai/struct/exprlang/DataType.scala
@@ -7,5 +7,5 @@ object DataType {
   case object BooleanType extends BaseType
   case class ArrayType(elType: BaseType) extends BaseType
   case class MapType(keyType: BaseType, valueType: BaseType) extends BaseType
-  case object ObjType extends BaseType
+  case class UserType(name: String) extends BaseType
 }
