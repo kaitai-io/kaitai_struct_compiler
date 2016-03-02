623d75e3581bf938b7d7a13d27cf1c7fa1fa754f
Mikhail Yakshin <greycat@altlinux.org>
2016-03-02 14:53:29 +0300

    ArrayType and MapType hold inner stuff type info

--- src/main/scala/io/kaitai/struct/exprlang/DataType.scala
+++ src/main/scala/io/kaitai/struct/exprlang/DataType.scala
@@ -5,6 +5,7 @@ object DataType {
   case object IntType extends BaseType
   case object StrType extends BaseType
   case object BooleanType extends BaseType
-  case object ArrayType extends BaseType
+  case class ArrayType(elType: BaseType) extends BaseType
+  case class MapType(keyType: BaseType, valueType: BaseType) extends BaseType
   case object ObjType extends BaseType
 }
