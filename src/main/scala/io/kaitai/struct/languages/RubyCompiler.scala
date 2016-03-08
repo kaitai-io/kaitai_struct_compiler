9086c4fa4349eab2892f0ca7f73891115c428798
Mikhail Yakshin <greycat@altlinux.org>
2016-03-08 20:04:56 +0300

    RubyCompiler: fixed to remove minor warnings

--- src/main/scala/io/kaitai/struct/languages/RubyCompiler.scala
+++ src/main/scala/io/kaitai/struct/languages/RubyCompiler.scala
@@ -3,17 +3,17 @@ package io.kaitai.struct.languages
 import io.kaitai.struct.exprlang.Ast
 import io.kaitai.struct.exprlang.Ast.expr
 import io.kaitai.struct.exprlang.DataType._
-import io.kaitai.struct.format.{AttrLikeSpec, AttrSpec, ProcessExpr, ProcessXor}
+import io.kaitai.struct.format.{AttrLikeSpec, ProcessExpr, ProcessXor}
 import io.kaitai.struct.translators.{BaseTranslator, RubyTranslator, TypeProvider}
 
 class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(verbose, outDir) with UpperCamelCaseClasses with EveryReadIsExpression {
   override def getTranslator(tp: TypeProvider): BaseTranslator = new RubyTranslator(tp)
 
-  override def outFileName(topClassName: String): String = s"${topClassName}.rb"
+  override def outFileName(topClassName: String): String = s"$topClassName.rb"
   override def indent: String = "  "
 
   override def fileHeader(sourceFileName: String, topClassName: String): Unit = {
-    out.puts(s"# This file was generated from '${sourceFileName}' with kaitai_struct compiler")
+    out.puts(s"# This file was generated from '$sourceFileName' with kaitai_struct compiler")
     out.puts
     out.puts("require 'kaitai_struct'")
     out.puts
@@ -52,15 +52,15 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
   override def attributeDeclaration(attrName: String, attrType: BaseType, isArray: Boolean): Unit = {}
 
   override def attributeReader(attrName: String, attrType: BaseType, isArray: Boolean): Unit = {
-    out.puts(s"attr_reader :${attrName}")
+    out.puts(s"attr_reader :$attrName")
   }
 
   override def attrFixedContentsParse(attrName: String, contents: Array[Byte]): Unit = {
-    out.puts(s"@${attrName} = ensure_fixed_contents(${contents.size}, [${contents.map(x => x.toInt & 0xff).mkString(", ")}])")
+    out.puts(s"@$attrName = ensure_fixed_contents(${contents.length}, [${contents.map(x => x.toInt & 0xff).mkString(", ")}])")
   }
 
   override def attrUserTypeParse(id: String, attrType: UserType, attr: AttrLikeSpec, io: String): Unit =
-    handleAssignment(id, attr, s"${type2class(attrType.name)}.new(${io}, self, @_root)", io)
+    handleAssignment(id, attr, s"${type2class(attrType.name)}.new($io, self, @_root)", io)
 
   override def attrProcess(proc: ProcessExpr, varSrc: String, varDest: String): Unit = {
     out.puts(proc match {
@@ -72,12 +72,12 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
   override def normalIO: String = "@_io"
 
   override def allocateIO(varName: String): String = {
-    out.puts(s"io = StringIO.new(@${varName})")
+    out.puts(s"io = StringIO.new(@$varName)")
     "io"
   }
 
   override def seek(io: String, pos: Ast.expr): Unit = {
-    out.puts(s"${io}.seek(${expression(pos)})")
+    out.puts(s"$io.seek(${expression(pos)})")
   }
 
   override def condIfHeader(expr: Ast.expr): Unit = {
@@ -90,19 +90,19 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
   }
 
   override def condRepeatEosHeader(id: String, io: String, dataType: BaseType): Unit = {
-    out.puts(s"@${id} = []")
-    out.puts(s"while not ${io}.eof?")
+    out.puts(s"@$id = []")
+    out.puts(s"while not $io.eof?")
     out.inc
   }
   override def handleAssignmentRepeatEos(id: String, expr: String): Unit =
-    out.puts(s"@${id} << ${expr}")
+    out.puts(s"@$id << $expr")
   override def condRepeatEosFooter: Unit = {
     out.dec
     out.puts("end")
   }
 
   override def condRepeatExprHeader(id: String, io: String, dataType: BaseType, repeatExpr: expr): Unit = {
-    out.puts(s"@${id} = Array.new(${expression(repeatExpr)}) {")
+    out.puts(s"@$id = Array.new(${expression(repeatExpr)}) {")
     out.inc
   }
   override def handleAssignmentRepeatExpr(id: String, expr: String): Unit = out.puts(expr)
@@ -112,7 +112,7 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
   }
 
   override def handleAssignmentSimple(id: String, expr: String): Unit =
-    out.puts(s"@${id} = ${expr}")
+    out.puts(s"@$id = $expr")
 
   override def stdTypeParseExpr(dataType: BaseType): String = {
     dataType match {
@@ -124,7 +124,7 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
       case StrEosType(encoding) =>
         "read_str_eos(\"" + encoding + "\")"
       case StrZType(encoding, terminator, include, consume, eosError) =>
-        "read_strz(\"" + encoding + '"' + s", ${terminator}, ${include}, ${consume}, ${eosError})"
+        "read_strz(\"" + encoding + '"' + s", $terminator, $include, $consume, $eosError)"
       case EnumType(enumName, t) =>
         s"${value2Const(enumName)}[read_${t.apiCall}]"
     }
@@ -135,7 +135,7 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
   override def noTypeWithSizeEosExpr: String = s"@_io.read"
 
   override def instanceHeader(className: String, instName: String, dataType: BaseType, isArray: Boolean): Unit = {
-    out.puts(s"def ${instName}")
+    out.puts(s"def $instName")
     out.inc
   }
 
@@ -144,11 +144,11 @@ class RubyCompiler(verbose: Boolean, outDir: String) extends LanguageCompiler(ve
   override def instanceFooter: Unit = classFooter()
 
   override def instanceCheckCacheAndReturn(instName: String): Unit = {
-    out.puts(s"return @${instName} if @${instName}")
+    out.puts(s"return @$instName if @$instName")
   }
 
   override def instanceReturn(instName: String): Unit = {
-    out.puts(s"@${instName}")
+    out.puts(s"@$instName")
   }
 
   override def instanceCalculate(instName: String, value: expr): Unit = {
