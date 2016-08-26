c20661acadaf23183ceae896db95c9ea69a6c524
Mikhail Yakshin <greycat@altlinux.org>
2016-08-27 01:35:20 +0300

    Started PerlTranslator, cloning from PythonTranslator

---
+++ shared/src/main/scala/io/kaitai/struct/translators/PerlTranslator.scala
@@ -0,0 +1,45 @@
+package io.kaitai.struct.translators
+
+import io.kaitai.struct.Utils
+import io.kaitai.struct.exprlang.Ast
+import io.kaitai.struct.exprlang.Ast.expr
+import io.kaitai.struct.exprlang.DataType.{BaseType, Int1Type}
+
+class PerlTranslator(provider: TypeProvider) extends BaseTranslator(provider) {
+  override def doBoolLiteral(n: Boolean): String = if (n) "1" else "0"
+
+  override def doLocalName(s: String) = {
+    s match {
+      case "_" => "$_"
+      case _ => s"$$self->${doName(s)}"
+    }
+  }
+
+  override def doName(s: String) = {
+    s"{$s}"
+  }
+
+  override def doEnumByLabel(enumType: String, label: String): String =
+    s"$$self->{_root}->{${Utils.upperCamelCase(enumType)}}->{$label}"
+
+  override def doSubscript(container: expr, idx: expr): String =
+    s"${translate(container)}[${translate(idx)}]"
+  override def doIfExp(condition: expr, ifTrue: expr, ifFalse: expr): String =
+    s"${translate(condition)} ? ${translate(ifTrue)} : ${translate(ifFalse)}"
+
+  // Predefined methods of various types
+  override def strToInt(s: Ast.expr, base: Ast.expr): String =
+    ???
+  override def strLength(value: Ast.expr): String =
+    s"len(${translate(value)})"
+  override def strSubstring(s: Ast.expr, from: Ast.expr, to: Ast.expr): String =
+    s"${translate(s)}[${translate(from)}:${translate(to)}]"
+
+  override def arrayFirst(a: expr): String =
+    s"${translate(a)}[0]"
+  override def arrayLast(a: expr): String =
+    s"${translate(a)}[-1]"
+
+  override def kaitaiStreamSize(value: Ast.expr): String =
+    s"${translate(value)}->size()"
+}
