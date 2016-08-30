68a1d5d6308e329c5342590a36180bd20f73c8ea
Mikhail Yakshin <greycat@altlinux.org>
2016-08-30 19:40:09 +0300

    RubyTranslator: made strToInt more intricate to work with special case of literal to-decimal conversion

--- shared/src/main/scala/io/kaitai/struct/translators/RubyTranslator.scala
+++ shared/src/main/scala/io/kaitai/struct/translators/RubyTranslator.scala
@@ -18,8 +18,13 @@ class RubyTranslator(provider: TypeProvider) extends BaseTranslator(provider) {
     s"${translate(condition)} ? ${translate(ifTrue)} : ${translate(ifFalse)}"
 
   // Predefined methods of various types
-  override def strToInt(s: expr, base: expr): String =
-    s"${translate(s)}.to_i(${translate(base)})"
+  override def strToInt(s: expr, base: expr): String = {
+    val baseStr = translate(base)
+    translate(s) + ".to_i" + (baseStr match {
+      case "10" => ""
+      case _ => s"($baseStr)"
+    })
+  }
   override def strLength(s: expr): String =
     s"${translate(s)}.size"
   override def strSubstring(s: expr, from: expr, to: expr): String =
