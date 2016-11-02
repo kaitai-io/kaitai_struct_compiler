841d468bf492f745d10a5d1c6f285d985081899f
Mikhail Yakshin <greycat@altlinux.org>
2016-11-02 12:27:27 +0300

    Added extra helper to help with unexpected data types

--- shared/src/main/scala/io/kaitai/struct/format/YAMLParseException.scala
+++ shared/src/main/scala/io/kaitai/struct/format/YAMLParseException.scala
@@ -2,3 +2,9 @@ package io.kaitai.struct.format
 
 class YAMLParseException(msg: String, path: List[String])
   extends RuntimeException(s"/${path.mkString("/")}: $msg", null)
+
+object YAMLParseException {
+  def badType(expected: String, got: Any, path: List[String]): YAMLParseException = {
+    new YAMLParseException(s"expected $expected, got $got (${got.getClass})", path)
+  }
+}
