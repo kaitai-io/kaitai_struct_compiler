b36b9c63644ff33934f1f7da978741915b5e9b1c
Mikhail Yakshin <greycat@altlinux.org>
2016-11-26 20:46:23 +0300

    Added EnumSpec to represent enums after parsing

---
+++ shared/src/main/scala/io/kaitai/struct/format/EnumSpec.scala
@@ -0,0 +1,21 @@
+package io.kaitai.struct.format
+
+case class EnumSpec(map: Map[Long, String]) {
+  var name = List[String]()
+
+  /**
+    * Stabilize order of generated enums by sorting it by integer ID - it
+    * both looks nicer and doesn't screw diffs in generated code.
+    */
+  lazy val sortedSeq: Seq[(Long, String)] = map.toSeq.sortBy(_._1)
+}
+
+object EnumSpec {
+  def fromYaml(src: Any, path: List[String]): EnumSpec = {
+    val srcMap = ParseUtils.asMap(src, path)
+    EnumSpec(srcMap.map { case (id, name) =>
+      val idLong = ParseUtils.asLong(id, path)
+      idLong -> ParseUtils.asStr(name, path ++ List(idLong.toString))
+    })
+  }
+}
