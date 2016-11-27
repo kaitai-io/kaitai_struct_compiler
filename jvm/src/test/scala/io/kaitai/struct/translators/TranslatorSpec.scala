9e3bb817345d9272f4e7797c72ba618eb22909e1
Mikhail Yakshin <greycat@altlinux.org>
2016-11-27 17:44:07 +0300

    Added some Perl expected results

--- jvm/src/test/scala/io/kaitai/struct/translators/TranslatorSpec.scala
+++ jvm/src/test/scala/io/kaitai/struct/translators/TranslatorSpec.scala
@@ -179,6 +179,7 @@ class TranslatorSpec extends FunSuite with TableDrivenPropertyChecks {
       CSharpCompiler -> "new byte[] { 34, 0, 10, 64, 65, 66, 92 }",
       JavaCompiler -> "new byte[] { 34, 0, 10, 64, 65, 66, 92 }",
       JavaScriptCompiler -> "[34, 0, 10, 64, 65, 66, 92]",
+      PerlCompiler -> "pack('C*', (34, 0, 10, 64, 65, 66, 92))",
       PHPCompiler -> "\"\\x22\\x00\\x0A\\x40\\x41\\x42\\x5C\"",
       PythonCompiler -> "struct.pack('7b', 34, 0, 10, 64, 65, 66, 92)",
       RubyCompiler -> "[34, 0, 10, 64, 65, 66, 92].pack('C*')"
@@ -189,6 +190,7 @@ class TranslatorSpec extends FunSuite with TableDrivenPropertyChecks {
       CSharpCompiler -> "new byte[] { 255, 0, 255 }",
       JavaCompiler -> "new byte[] { -1, 0, -1 }",
       JavaScriptCompiler -> "[255, 0, 255]",
+      PerlCompiler -> "pack('C*', (255, 0, 255))",
       PHPCompiler -> "\"\\xFF\\x00\\xFF\"",
       PythonCompiler -> "struct.pack('3b', -1, 0, -1)",
       RubyCompiler -> "[255, 0, 255].pack('C*')"
