e3246f6156056dae4fa788d868928b00b0dca719
Mikhail Yakshin <greycat@altlinux.org>
2017-01-08 12:45:13 +0300

    PHPCompiler: implemented BitsType support

--- shared/src/main/scala/io/kaitai/struct/languages/PHPCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/PHPCompiler.scala
@@ -231,6 +231,10 @@ class PHPCompiler(config: RuntimeConfig, out: LanguageOutputWriter)
         s"$io->readBytes(${expression(size)})"
       case BytesEosType(_) =>
         s"$io->readBytesFull()"
+      case BitsType(1) =>
+        s"$io->readBitsInt(1) != 0"
+      case BitsType(width: Int) =>
+        s"$io->readBitsInt($width)"
       case t: UserType =>
         val addArgs = if (!t.isOpaque) s", $$this, ${privateMemberName(RootIdentifier)}" else ""
         s"new ${types2classAbs(t.classSpec.get.name)}($io$addArgs)"
