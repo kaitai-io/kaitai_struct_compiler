4250d5994117556af149d9f53e9353c18b09a671
Mikhail Yakshin <greycat@altlinux.org>
2017-02-08 19:54:19 +0300

    KSVersion: added C-style dumping to int + tests

--- shared/src/main/scala/io/kaitai/struct/format/KSVersion.scala
+++ shared/src/main/scala/io/kaitai/struct/format/KSVersion.scala
@@ -14,6 +14,26 @@ case class KSVersion(nums: List[Int]) extends Ordered[KSVersion] {
   }
 
   override def toString: String = nums.mkString(".")
+
+  /**
+    * Dumps a version in C-style integer, that is 1.2.3 becomes 001_002_003 = 1002003.
+    * These versions can be compared to each other like regular numbers. Values for
+    * each individual version component span from 0 to 999 inclusive, thus maximal ever
+    * possible value is 999_999_999, which fits into 32-bit signed integer.
+    * @return version as an integer, allowing easy numeric comparison
+    */
+  def toInt: Int = {
+    if (nums.size > 3)
+      throw new RuntimeException(s"C-style version int can have max 3 components, but $this is given")
+    val nums2 = nums ++ List.fill(3 - nums.size)(0)
+    nums2.foldLeft(0) { (sum, comp) =>
+      if (comp < 0 || comp > 999) {
+        throw new RuntimeException(s"C-style version int only allows components [0..999], but $comp was used")
+      } else {
+        sum * 1000 + comp
+      }
+    }
+  }
 }
 
 object KSVersion {
