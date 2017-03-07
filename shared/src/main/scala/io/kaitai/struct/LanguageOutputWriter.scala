9aa8e7d419908b41bf8d26e25fbd0570af6cecc1
Mikhail Yakshin <greycat@altlinux.org>
2017-03-07 13:08:30 +0300

    Extracted FileLanguageOutputWriter into JVM-only src dir

--- shared/src/main/scala/io/kaitai/struct/LanguageOutputWriter.scala
+++ shared/src/main/scala/io/kaitai/struct/LanguageOutputWriter.scala
@@ -1,8 +1,5 @@
 package io.kaitai.struct
 
-import java.io.{File, FileOutputStream, OutputStreamWriter, PrintWriter}
-import java.nio.charset.StandardCharsets
-
 abstract class LanguageOutputWriter(indentStr: String) {
   var indentLevel = 0
 
@@ -27,17 +24,6 @@ abstract class LanguageOutputWriter(indentStr: String) {
       puts(s"$prefix$line")
     )
   }
-}
-
-class FileLanguageOutputWriter(fileName: String, indentStr: String) extends LanguageOutputWriter(indentStr) {
-  private val outDir = new File(fileName).getParentFile()
-  outDir.mkdirs
-  private val out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8), true)
-
-  override def add(other: StringLanguageOutputWriter) = out.write(other.result)
-  override def puts(s: String): Unit = out.println(indentNow + s)
-  override def puts: Unit = out.println
-  override def close = out.close
 }
 
 class StringLanguageOutputWriter(indentStr: String) extends LanguageOutputWriter(indentStr) {
