a4f7802da6098f4d9a44dc38bb02ef0a27a59094
Mikhail Yakshin <greycat@altlinux.org>
2017-04-24 21:59:40 +0300

    GoClassCompiler: adjusted order of instance declarations

--- shared/src/main/scala/io/kaitai/struct/GoClassCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/GoClassCompiler.scala
@@ -25,6 +25,9 @@ class GoClassCompiler(
     // Basic struct declaration
     lang.classHeader(curClass.name)
     compileAttrDeclarations(curClass.seq ++ extraAttrs)
+    curClass.instances.foreach { case (instName, instSpec) =>
+      compileInstanceDeclaration(instName, instSpec)
+    }
     lang.classFooter(curClass.name)
 
     // Read method
@@ -45,5 +48,28 @@ class GoClassCompiler(
 
     // Recursive types
     compileSubclasses(curClass)
+  }
+
+  override def compileInstance(className: List[String], instName: InstanceIdentifier, instSpec: InstanceSpec, extraAttrs: ListBuffer[AttrSpec]): Unit = {
+    // Determine datatype
+    val dataType = instSpec.dataTypeComposite
+
+    if (!instSpec.doc.isEmpty)
+      lang.attributeDoc(instName, instSpec.doc)
+    lang.instanceHeader(className, instName, dataType)
+    lang.instanceCheckCacheAndReturn(instName)
+
+    instSpec match {
+      case vi: ValueInstanceSpec =>
+        lang.attrParseIfHeader(instName, vi.ifExpr)
+        lang.instanceCalculate(instName, dataType, vi.value)
+        lang.attrParseIfFooter(vi.ifExpr)
+      case i: ParseInstanceSpec =>
+        lang.attrParse(i, instName, extraAttrs)
+    }
+
+    lang.instanceSetCalculated(instName)
+    lang.instanceReturn(instName)
+    lang.instanceFooter
   }
 }
