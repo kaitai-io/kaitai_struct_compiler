241aad69fe60fcdecc802acdcb60307712b5c872
Mikhail Yakshin <greycat@altlinux.org>
2017-04-24 20:02:43 +0300

    Added --go-package runtime config option

--- shared/src/main/scala/io/kaitai/struct/RuntimeConfig.scala
+++ shared/src/main/scala/io/kaitai/struct/RuntimeConfig.scala
@@ -3,6 +3,7 @@ package io.kaitai.struct
 case class RuntimeConfig(
   debug: Boolean = false,
   opaqueTypes: Boolean = false,
+  goPackage: String = "",
   javaPackage: String = "",
   dotNetNamespace: String = "Kaitai",
   phpNamespace: String = ""
