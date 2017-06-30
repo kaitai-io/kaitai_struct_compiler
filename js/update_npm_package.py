e8b1ce76427fd84e0eb7ffaff060d806c53d613e
KT <koczkatamas@gmail.com>
2017-07-01 01:08:48 +0200

    Add npm compilation helper

---
+++ js/update_npm_package.py
@@ -0,0 +1,26 @@
+moduleTemplate = '''
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.KaitaiStructCompiler = factory();
+  }
+}(this, function () {
+    
+var exports = {};
+var __ScalaJSEnv = { exportsNamespace: exports };
+
+{{compilerCode}}
+
+return exports.io.kaitai.struct.MainJs;
+
+}));
+'''.lstrip()
+
+with open('target/scala-2.11/kaitai-struct-compiler-fastopt.js','rt') as f: compilerCode = f.read()
+
+moduleCode = moduleTemplate.replace('{{compilerCode}}', compilerCode)
+
+with open('npm/kaitai-struct-compiler.js','wt') as f: f.write(moduleCode)
