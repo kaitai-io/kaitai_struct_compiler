dcff5be95c3c2395247d0c4543202029df0a6676
Mikhail Yakshin <greycat@altlinux.org>
2017-08-08 22:27:45 +0300

    Update update_npm_package.py to adhere to file placement and build process changes

--- js/update_npm_package.py
+++ js/update_npm_package.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python2
 
+import os
 import shutil
 import re
 import subprocess
 import datetime
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
 moduleTemplate = '''
 (function (root, factory) {
@@ -30,15 +40,17 @@ with open('target/scala-2.11/kaitai-struct-compiler-fastopt.js','rt') as f: comp
 
 moduleCode = moduleTemplate.replace('{{compilerCode}}', compilerCode)
 
+mkdir_p('npm')
+
 with open('npm/kaitai-struct-compiler.js','wt') as f: f.write(moduleCode)
-for fn in ['LICENSE', 'README.md']:
-    shutil.copy('../%s' % fn, 'npm/')
+for fn in ['../LICENSE', 'README.md']:
+    shutil.copy(fn, 'npm/')
 
 gitInfo = subprocess.check_output(['git log -1 --format=%H,%ct'], shell=True).strip().split(',')
 commitId = gitInfo[0]
 commitTs = int(gitInfo[1])
 commitDate = datetime.datetime.fromtimestamp(commitTs).strftime('%Y%m%d.%H%M%S')
 
-with open('npm/package.json','rb') as f: packageJson = f.read()
+with open('package.json','rb') as f: packageJson = f.read()
 packageJson = re.sub(r'("version": "\d+\.\d+\.\d+-SNAPSHOT.)[^"]*"', r'\g<1>%s"' % commitDate, packageJson)
 with open('npm/package.json','wb') as f: f.write(packageJson)
