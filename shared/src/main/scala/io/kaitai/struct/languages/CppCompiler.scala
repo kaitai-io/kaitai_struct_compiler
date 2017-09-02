f9cec5467cbfbc3ad3e7fc6099e4b7215150abb1
Thomas Tempelmann <tempelmann@gmail.com>
2017-09-02 09:52:51 +0200

    #250: Uses "not" and "and" instead of "!" and "&&" operators for readability.

--- shared/src/main/scala/io/kaitai/struct/languages/CppCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/CppCompiler.scala
@@ -284,7 +284,7 @@ class CppCompiler(
     }
 
     val checkNull = if (attr.isNullableSwitchRaw) {
-      Some(s"!${nullFlagForName(id)}")
+      Some(s"not ${nullFlagForName(id)}")
     } else {
       None
     }
@@ -292,7 +292,7 @@ class CppCompiler(
     val checks: List[String] = List(checkLazy, checkNull).flatten
 
     if (checks.nonEmpty) {
-      outSrc.puts(s"if (${checks.mkString(" && ")}) {")
+      outSrc.puts(s"if (${checks.mkString(" and ")}) {")
       outSrc.inc
     }
 
@@ -494,7 +494,7 @@ class CppCompiler(
     outSrc.puts("{")
     outSrc.inc
     outSrc.puts("int i = 0;")
-    outSrc.puts(s"while (!$io->is_eof()) {")
+    outSrc.puts(s"while (not $io->is_eof()) {")
     outSrc.inc
   }
 
@@ -568,7 +568,7 @@ class CppCompiler(
     typeProvider._currentIteratorType = Some(dataType)
     outSrc.puts("i++;")
     outSrc.dec
-    outSrc.puts(s"} while (!(${expression(untilExpr)}));")
+    outSrc.puts(s"} while (not (${expression(untilExpr)}));")
     outSrc.dec
     outSrc.puts("}")
   }
