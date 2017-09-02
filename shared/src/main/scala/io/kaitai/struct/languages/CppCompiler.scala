5fc9c75aab40564465cb46573c9603af0cdd9964
Thomas Tempelmann <tempelmann@gmail.com>
2017-09-02 09:35:18 +0200

    Changes imports from system to user paths by using quotes instead of angle brackets. (reverted from commit 0b27869146f6c96540ca37fd0b7e2e2a13e19ee6)

--- shared/src/main/scala/io/kaitai/struct/languages/CppCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/CppCompiler.scala
@@ -62,7 +62,8 @@ class CppCompiler(
     outHdrHeader.puts
     outHdrHeader.puts(s"// $headerComment")
     outHdrHeader.puts
-    outHdrHeader.puts("#include \"kaitai/kaitaistruct.h\"")
+    outHdrHeader.puts("#include <kaitai/kaitaistruct.h>")
+    outHdrHeader.puts("#include <kaitai/kaitaistream.h>")
     outHdrHeader.puts
 
     importListHdr.add("stdint.h")
