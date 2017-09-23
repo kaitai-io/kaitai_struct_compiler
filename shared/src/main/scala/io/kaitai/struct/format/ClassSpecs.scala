f9de27022a54e1473e6242c8589cdb5d7ca0aab3
Mikhail Yakshin <greycat@altlinux.org>
2017-09-23 12:55:49 +0300

    Added forEachRec for ClassSpecs

--- shared/src/main/scala/io/kaitai/struct/format/ClassSpecs.scala
+++ shared/src/main/scala/io/kaitai/struct/format/ClassSpecs.scala
@@ -12,6 +12,15 @@ import scala.concurrent.Future
 abstract class ClassSpecs(val firstSpec: ClassSpec) extends mutable.HashMap[String, ClassSpec] {
   this(firstSpec.name.head) = firstSpec
 
+  /**
+    * Calls certain function on all [[ClassSpec]] elements stored in this ClassSpecs,
+    * and all subtypes stored in these elements, recursively.
+    */
+  def forEachRec(proc: (ClassSpec) => ()): Unit = {
+    // FIXME: grab exception and rethrow more localized one, with a specName?
+    foreach { case (_, typeSpec) => typeSpec.forEachRec(proc) }
+  }
+
   def importRelative(name: String, path: List[String], inFile: Option[String]): Future[Option[ClassSpec]]
   def importAbsolute(name: String, path: List[String], inFile: Option[String]): Future[Option[ClassSpec]]
 }
