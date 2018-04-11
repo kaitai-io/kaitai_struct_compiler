d6e6f0be37d5880225634d93ba170635407d0172
Connor Wood <connorwood71@gmail.com>
2018-04-11 18:00:00 +0100

    Fixed ternary operator to if expression

--- shared/src/main/scala/io/kaitai/struct/translators/RustTranslator.scala
+++ shared/src/main/scala/io/kaitai/struct/translators/RustTranslator.scala
@@ -62,7 +62,9 @@ class RustTranslator(provider: TypeProvider, config: RuntimeConfig) extends Base
   override def doSubscript(container: expr, idx: expr): String =
     s"${translate(container)}[${translate(idx)}]"
   override def doIfExp(condition: expr, ifTrue: expr, ifFalse: expr): String =
-    s"(${translate(condition)} ? ${translate(ifTrue)} : ${translate(ifFalse)})"
+    "if " + translate(condition) +
+    	" { " + translate(ifTrue) + " } else { " +
+	translate(ifFalse) + "}"
 
   // Predefined methods of various types
   override def strConcat(left: Ast.expr, right: Ast.expr): String =
