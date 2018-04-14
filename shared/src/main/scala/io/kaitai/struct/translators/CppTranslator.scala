80e0ba7fd39e66687300b8a3ebb65f08a67eb972
Mikhail Yakshin <greycat@altlinux.org>
2018-04-14 16:16:06 +0100

    CppTranslator: major fix for proper generation of C++ integer literals

--- shared/src/main/scala/io/kaitai/struct/translators/CppTranslator.scala
+++ shared/src/main/scala/io/kaitai/struct/translators/CppTranslator.scala
@@ -13,6 +13,58 @@ import io.kaitai.struct.languages.CppCompiler
 class CppTranslator(provider: TypeProvider, importListSrc: ImportList) extends BaseTranslator(provider) {
   val CHARSET_UTF8 = Charset.forName("UTF-8")
 
+  val MAX_UINT64 = BigInt("18446744073709551615")
+
+  /**
+    * Handles integer literals for C++ by appending relevant suffix to
+    * decimal notation.
+    *
+    * Note that suffixes essentially mean "long", "unsigned long",
+    * and "unsigned long long", which are not really guaranteed to match
+    * `int32_t`, `uint32_t` and `uint64_t`, but it would work for majority
+    * of current compilers.
+    *
+    * For reference, ranges of integers that are used in this conversion are:
+    *
+    * * int32_t (no suffix): –2147483648..2147483647
+    * * uint32_t (UL): 0..4294967295
+    * * int64_t (LL): -9223372036854775808..9223372036854775807
+    * * uint64_t (ULL): 0..18446744073709551615
+    *
+    * Merging all these ranges, we get the following decision tree:
+    *
+    * * -9223372036854775808..-2147483649 => LL
+    * * -2147483648..2147483647 => no suffix
+    * * 2147483648..4294967295 => UL
+    * * 4294967296..9223372036854775807 => LL
+    * * 9223372036854775808..18446744073709551615 => ULL
+    *
+    * Beyond these boundaries, C++ is unlikely to be able to represent
+    * these anyway, so we just drop the suffix and hope for the miracle.
+    *
+    * @param n integer to render
+    * @return rendered integer literal in C++ syntax as string
+    */
+  override def doIntLiteral(n: BigInt): String = {
+    val suffix = if (n < -9223372036854775808L) {
+      "" // too low, no suffix would help anyway
+    } else if (n <= -2147483649L) {
+      "LL" // -9223372036854775808..–2147483649
+    } else if (n <= 2147483647L) {
+      "" // -2147483648..2147483647
+    } else if (n <= 4294967295L) {
+      "UL" // 2147483648..4294967295
+    } else if (n <= 9223372036854775807L) {
+      "LL" // 4294967296..9223372036854775807
+    } else if (n <= MAX_UINT64) {
+      "ULL" // 9223372036854775808..18446744073709551615
+    } else {
+      "" // too high, no suffix would help anyway
+    }
+
+    s"$n$suffix"
+  }
+
   /**
     * Handles string literal for C++ by wrapping a C `const char*`-style string
     * into a std::string constructor. Note that normally std::string
