944c2b8fa6447650809fac3ad1736af27837c378
KOLANICH <KOLANICH@users.noreply.github.com>
2019-01-27 11:10:40 +0000

    Added a command to build my docker image

--- publish_deb_to_bintray.sh
+++ publish_deb_to_bintray.sh
@@ -8,7 +8,7 @@ BINTRAY_ACCOUNT=kaitai-io
 BINTRAY_REPO=debian_unstable
 BINTRAY_PACKAGE=kaitai-struct-compiler
 BINTRAY_VERSION="$KAITAI_STRUCT_VERSION"
-# BINTRAY_API_KEY comes from encrypted variables from web UI
+# BINTRAY_API_KEY and KAITAI_IMAGE_BUILD_HOOK_TOKEN come from encrypted variables from web UI
 
 BINTRAY_DEB_DISTRIBUTION=jessie
 BINTRAY_DEB_ARCH=all
@@ -19,3 +19,4 @@ BINTRAY_DEB_COMPONENT=main
 bintray_create_version
 bintray_upload_deb "jvm/target/kaitai-struct-compiler_${KAITAI_STRUCT_VERSION}_all.deb"
 bintray_publish_version
+if [ ! -z "$KAITAI_IMAGE_BUILD_HOOK_TOKEN" ]; then curl -F "token=${KAITAI_IMAGE_BUILD_HOOK_TOKEN}" -F ref=master" https://gitlab.com/api/v4/projects/10444125/trigger/pipeline || true; fi;
