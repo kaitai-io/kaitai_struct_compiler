21750277f393ee03be2b154da9ac5ca58e2cf79c
Petr Pucil <petr.pucil@seznam.cz>
2020-08-23 23:26:28 +0200

    Revert "Construct: add header comment"
    This reverts commit bd4956499a6cb41d4444aeea86bdaf51103519cc.

--- shared/src/main/scala/io/kaitai/struct/ConstructClassCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/ConstructClassCompiler.scala
@@ -15,8 +15,6 @@ class ConstructClassCompiler(classSpecs: ClassSpecs, topClass: ClassSpec) extend
   val translator = new ConstructTranslator(provider, importList)
 
   override def compile: CompileLog.SpecSuccess = {
-    out.puts(s"# $headerComment")
-    out.puts
     out.puts("from construct import *")
     out.puts("from construct.lib import *")
     out.puts
