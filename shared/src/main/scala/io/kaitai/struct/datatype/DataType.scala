kaitai-io/kaitai_struct_compiler

commit 19340b90ae5d50c003c1fe0d0af9d59fce0f5cd8

Add bitEndian param to bit types, fix all usages

shared/src/main/scala/io/kaitai/struct/datatype/DataType.scala
@@ -46,8 +46,8 @@ object DataType {
       s"$ch1${width.width}${finalEnd.map(_.toSuffix).getOrElse("")}"
     }
   }
-  case object BitsType1 extends BooleanType
-  case class BitsType(width: Int) extends IntType
+  case class BitsType1(bitEndian: BitEndianness) extends BooleanType
+  case class BitsType(width: Int, bitEndian: BitEndianness) extends IntType
 
   abstract class FloatType extends NumericType
   case object CalcFloatType extends FloatType
@@ -293,7 +293,7 @@ object DataType {
 
   private val ReIntType = """([us])(2|4|8)(le|be)?""".r
   private val ReFloatType = """f(4|8)(le|be)?""".r
-  private val ReBitType = """b(\d+)""".r
+  private val ReBitType = """b(\d+)(le|be)?""".r
   private val ReUserTypeWithArgs = """^((?:[a-z][a-z0-9_]*(?:::)?)+)\((.*)\)$""".r
 
   def fromYaml(
@@ -332,14 +332,19 @@ object DataType {
             },
             Endianness.fromString(Option(endianStr), metaDef.endian, dt, path)
           )
-        case ReBitType(widthStr) =>
+        case ReBitType(widthStr, bitEndianStr) =>
           (arg.enumRef, widthStr.toInt) match {
             case (None, 1) =>
               // if we're not inside enum and it's 1-bit type
-              BitsType1
+              BitsType1(
+                BitEndianness.fromString(Option(bitEndianStr), metaDef.bitEndian, dt, path)
+              )
             case (_, width) =>
               // either inside enum (any width) or (width != 1)
-              BitsType(width)
+              BitsType(
+                width,
+                BitEndianness.fromString(Option(bitEndianStr), metaDef.bitEndian, dt, path)
+              )
           }
         case "str" | "strz" =>
           val enc = getEncoding(arg.encoding, metaDef, path)
@@ -427,8 +432,9 @@ object DataType {
         )
       case ReBitType(widthStr) =>
         widthStr match {
-          case "1" => BitsType1
-          case _ => BitsType(widthStr.toInt)
+          // bit endianness is not applicable here (and the dependent code doesn't care about it), so why not assume big endian
+          case "1" => BitsType1(BigBitEndian)
+          case _ => BitsType(widthStr.toInt, BigBitEndian)
         }
       case "str" => CalcStrType
       case "bool" => CalcBooleanType

shared/src/main/scala/io/kaitai/struct/ClassCompiler.scala
@@ -362,7 +362,7 @@ class ClassCompiler(
 
   def isUnalignedBits(dt: DataType): Boolean =
     dt match {
-      case _: BitsType | BitsType1 => true
+      case _: BitsType | _: BitsType1 => true
       case et: EnumType => isUnalignedBits(et.basedOn)
       case _ => false
     }

shared/src/main/scala/io/kaitai/struct/GraphvizClassCompiler.scala
@@ -431,7 +431,7 @@ object GraphvizClassCompiler extends LanguageCompilerStatic {
         s"str($bytesStr$comma$encoding)"
       case EnumType(name, basedOn) =>
         s"${dataTypeName(basedOn)}→${type2display(name)}"
-      case BitsType(width) => s"b$width"
+      case BitsType(width, bitEndian) => s"b$width"
       case _ => dataType.toString
     }
   }

shared/src/main/scala/io/kaitai/struct/languages/CSharpCompiler.scala
@@ -372,9 +372,9 @@ class CSharpCompiler(val typeProvider: ClassTypeProvider, config: RuntimeConfig)
         s"$io.ReadBytesFull()"
       case BytesTerminatedType(terminator, include, consume, eosError, _) =>
         s"$io.ReadBytesTerm($terminator, $include, $consume, $eosError)"
-      case BitsType1 =>
+      case BitsType1(bitEndian) =>
         s"$io.ReadBitsInt(1) != 0"
-      case BitsType(width: Int) =>
+      case BitsType(width: Int, bitEndian) =>
         s"$io.ReadBitsInt($width)"
       case t: UserType =>
         val addParams = Utils.join(t.args.map((a) => translator.translate(a)), "", ", ", ", ")
@@ -633,7 +633,7 @@ object CSharpCompiler extends LanguageCompilerStatic
       case FloatMultiType(Width4, _) => "float"
       case FloatMultiType(Width8, _) => "double"
 
-      case BitsType(_) => "ulong"
+      case BitsType(_, _) => "ulong"
 
       case CalcIntType => "int"
       case CalcFloatType => "double"

shared/src/main/scala/io/kaitai/struct/languages/CppCompiler.scala
@@ -700,9 +700,9 @@ class CppCompiler(
         s"$io->read_bytes_full()"
       case BytesTerminatedType(terminator, include, consume, eosError, _) =>
         s"$io->read_bytes_term($terminator, $include, $consume, $eosError)"
-      case BitsType1 =>
+      case BitsType1(bitEndian) =>
         s"$io->read_bits_int(1)"
-      case BitsType(width: Int) =>
+      case BitsType(width: Int, bitEndian) =>
         s"$io->read_bits_int($width)"
       case t: UserType =>
         val addParams = Utils.join(t.args.map((a) => translator.translate(a)), "", ", ", ", ")
@@ -1038,7 +1038,7 @@ object CppCompiler extends LanguageCompilerStatic
       case FloatMultiType(Width4, _) => "float"
       case FloatMultiType(Width8, _) => "double"
 
-      case BitsType(_) => "uint64_t"
+      case BitsType(_, _) => "uint64_t"
 
       case _: BooleanType => "bool"
       case CalcIntType => "int32_t"

shared/src/main/scala/io/kaitai/struct/languages/GoCompiler.scala
@@ -399,9 +399,9 @@ class GoCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
         s"$io.ReadBytesFull()"
       case BytesTerminatedType(terminator, include, consume, eosError, _) =>
         s"$io.ReadBytesTerm($terminator, $include, $consume, $eosError)"
-      case BitsType1 =>
+      case BitsType1(bitEndian) =>
         s"$io.ReadBitsInt(1)"
-      case BitsType(width: Int) =>
+      case BitsType(width: Int, bitEndian) =>
         s"$io.ReadBitsInt($width)"
       case t: UserType =>
         val addArgs = if (t.isOpaque) {
@@ -587,7 +587,7 @@ object GoCompiler extends LanguageCompilerStatic
       case FloatMultiType(Width4, _) => "float32"
       case FloatMultiType(Width8, _) => "float64"
 
-      case BitsType(_) => "uint64"
+      case BitsType(_, _) => "uint64"
 
       case _: BooleanType => "bool"
       case CalcIntType => "int"

shared/src/main/scala/io/kaitai/struct/languages/JavaCompiler.scala
@@ -446,9 +446,9 @@ class JavaCompiler(val typeProvider: ClassTypeProvider, config: RuntimeConfig)
         s"$io.readBytesFull()"
       case BytesTerminatedType(terminator, include, consume, eosError, _) =>
         s"$io.readBytesTerm($terminator, $include, $consume, $eosError)"
-      case BitsType1 =>
+      case BitsType1(bitEndian) =>
         s"$io.readBitsInt(1) != 0"
-      case BitsType(width: Int) =>
+      case BitsType(width: Int, bitEndian) =>
         s"$io.readBitsInt($width)"
       case t: UserType =>
         val addArgs = if (t.isOpaque) {
@@ -800,7 +800,7 @@ object JavaCompiler extends LanguageCompilerStatic
       case FloatMultiType(Width4, _) => "float"
       case FloatMultiType(Width8, _) => "double"
 
-      case BitsType(_) => "long"
+      case BitsType(_, _) => "long"
 
       case _: BooleanType => "boolean"
       case CalcIntType => "int"
@@ -844,7 +844,7 @@ object JavaCompiler extends LanguageCompilerStatic
       case FloatMultiType(Width4, _) => "Float"
       case FloatMultiType(Width8, _) => "Double"
 
-      case BitsType(_) => "Long"
+      case BitsType(_, _) => "Long"
 
       case _: BooleanType => "Boolean"
       case CalcIntType => "Integer"

shared/src/main/scala/io/kaitai/struct/languages/JavaScriptCompiler.scala
@@ -383,9 +383,9 @@ class JavaScriptCompiler(val typeProvider: ClassTypeProvider, config: RuntimeCon
         s"$io.readBytesFull()"
       case BytesTerminatedType(terminator, include, consume, eosError, _) =>
         s"$io.readBytesTerm($terminator, $include, $consume, $eosError)"
-      case BitsType1 =>
+      case BitsType1(bitEndian) =>
         s"$io.readBitsInt(1) != 0"
-      case BitsType(width: Int) =>
+      case BitsType(width: Int, bitEndian) =>
         s"$io.readBitsInt($width)"
       case t: UserType =>
         val parent = t.forcedParent match {

commit fe09b44a1b72ddc0183d25e8ff3d803492defd82

Fix "b(\d+)" type identifier recognition
... which has been broken in commit 19340b90

shared/src/main/scala/io/kaitai/struct/datatype/DataType.scala
@@ -447,7 +447,7 @@ object DataType {
           },
           None
         )
-      case ReBitType(widthStr) =>
+      case ReBitType(widthStr, bitEndianStr) =>
         widthStr match {
           // bit endianness is not applicable here (and the dependent code doesn't care about it), so why not assume big endian
           case "1" => BitsType1(BigBitEndian)