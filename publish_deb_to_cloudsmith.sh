313075ef1d240a4cd0c3de7057b447a155a44606
Mikhail Yakshin <greycat@altlinux.org>
2021-02-06 14:25:40 +0000

    Added comment on original of auth var

--- publish_deb_to_cloudsmith.sh
+++ publish_deb_to_cloudsmith.sh
@@ -2,6 +2,7 @@
 
 pip install --upgrade cloudsmith-cli
 
+# CLOUDSMITH_API_KEY will come from secure environment variables
 cloudsmith push deb \
 	kaitai/debian-unstable/any-distro/any-version \
 	"jvm/target/kaitai-struct-compiler_${KAITAI_STRUCT_VERSION}_all.deb"
