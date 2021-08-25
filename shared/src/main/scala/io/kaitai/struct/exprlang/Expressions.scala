18db1553de4d443777525b65bbc9f49488b01b33
Mingun <alexander_sergey@mail.ru>
2021-08-25 15:16:32 +0500

    Unify style
    Change commenting pattern so stupid VSCode folding algorithm correcly fold class

--- shared/src/main/scala/io/kaitai/struct/exprlang/Expressions.scala
+++ shared/src/main/scala/io/kaitai/struct/exprlang/Expressions.scala
@@ -35,20 +35,20 @@ object Expressions {
   val FLOAT_NUMBER = Lexical.floatnumber
   val STRING: P[String] = Lexical.stringliteral
 
-  val test: P[Ast.expr] = P( or_test ~ ("?" ~ test ~ ":" ~ test).? ).map{
-      case (x, None) => x
-      case (condition, Some((ifTrue, ifFalse))) => Ast.expr.IfExp(condition, ifTrue, ifFalse)
-    }
-  val or_test = P( and_test.rep(1, kw("or")) ).map{
+  val test: P[Ast.expr] = P( or_test ~ ("?" ~ test ~ ":" ~ test).? ).map {
+    case (x, None) => x
+    case (condition, Some((ifTrue, ifFalse))) => Ast.expr.IfExp(condition, ifTrue, ifFalse)
+  }
+  val or_test = P( and_test.rep(1, kw("or")) ).map {
     case Seq(x) => x
     case xs => Ast.expr.BoolOp(Ast.boolop.Or, xs)
   }
-  val and_test = P( not_test.rep(1, kw("and")) ).map{
+  val and_test = P( not_test.rep(1, kw("and")) ).map {
     case Seq(x) => x
     case xs => Ast.expr.BoolOp(Ast.boolop.And, xs)
   }
   val not_test: P[Ast.expr] = P( (kw("not") ~ not_test).map(Ast.expr.UnaryOp(Ast.unaryop.Not, _)) | comparison )
-  val comparison: P[Ast.expr] = P( expr ~ (comp_op ~ expr).? ).map{
+  val comparison: P[Ast.expr] = P( expr ~ (comp_op ~ expr).? ).map {
     case (lhs, None) => lhs
     case (lhs, Some(chunks)) =>
       val (op, rhs) = chunks
@@ -69,7 +69,7 @@ object Expressions {
   val comp_op = P( LtE|GtE|Eq|Gt|Lt|NotEq )
   val Add = op("+", Ast.operator.Add)
   val Sub = op("-", Ast.operator.Sub)
-//  val Pow = op("**", Ast.operator.Pow)
+  // val Pow = op("**", Ast.operator.Pow)
   val Mult= op("*", Ast.operator.Mult)
   val Div = op("/", Ast.operator.Div)
   val Mod = op("%", Ast.operator.Mod)
@@ -77,7 +77,7 @@ object Expressions {
   val BitAnd = op("&", Ast.operator.BitAnd)
   val BitXor = op("^", Ast.operator.BitXor)
 
-  def Chain(p: P[Ast.expr], op: P[Ast.operator]) = P( p ~ (op ~ p).rep ).map{
+  def Chain(p: P[Ast.expr], op: P[Ast.operator]) = P( p ~ (op ~ p).rep ).map {
     case (lhs, chunks) =>
       chunks.foldLeft(lhs){case (lhs, (op, rhs)) =>
         Ast.expr.BinOp(lhs, op, rhs)
@@ -96,27 +96,27 @@ object Expressions {
     ("~" ~ factor).map(Ast.expr.UnaryOp(Ast.unaryop.Invert, _)) |
     power
   )
-//  val power: P[Ast.expr] = P( atom ~ trailer.rep ~ (Pow ~ factor).? ).map{
-//    case (lhs, trailers, rhs) =>
-//      val left = trailers.foldLeft(lhs)((l, t) => t(l))
-//      rhs match{
-//        case None => left
-//        case Some((op, right)) => Ast.expr.BinOp(left, op, right)
-//      }
-//  }
+  // val power: P[Ast.expr] = P( atom ~ trailer.rep ~ (Pow ~ factor).? ).map {
+  //   case (lhs, trailers, rhs) =>
+  //     val left = trailers.foldLeft(lhs)((l, t) => t(l))
+  //     rhs match{
+  //       case None => left
+  //       case Some((op, right)) => Ast.expr.BinOp(left, op, right)
+  //     }
+  // }
   val power: P[Ast.expr] = P( atom ~ trailer.rep ).map {
     case (lhs, trailers) =>
       trailers.foldLeft(lhs)((l, t) => t(l))
   }
   val atom: P[Ast.expr] = {
     val empty_list = ("[" ~ "]").map(_ => Ast.expr.List(Nil))
-//    val empty_dict = ("{" ~ "}").map(_ => Ast.expr.Dict(Nil, Nil))
+    // val empty_dict = ("{" ~ "}").map(_ => Ast.expr.Dict(Nil, Nil))
     P(
       empty_list |
-//      empty_dict |
+      // empty_dict |
       "(" ~ test ~ ")" |
       "[" ~ list ~ "]" |
-//      "{" ~ dictorsetmaker ~ "}" |
+      // "{" ~ dictorsetmaker ~ "}" |
       enumByName |
       byteSizeOfType |
       bitSizeOfType |
@@ -134,8 +134,8 @@ object Expressions {
   val list = P( list_contents ).map(Ast.expr.List(_))
 
   val trailer: P[Ast.expr => Ast.expr] = {
-    val call = P("(" ~ arglist ~ ")").map{ case (args) => (lhs: Ast.expr) => Ast.expr.Call(lhs, args)}
-    val slice = P("[" ~ test ~ "]").map{ case (args) => (lhs: Ast.expr) => Ast.expr.Subscript(lhs, args)}
+    val call = P("(" ~ arglist ~ ")").map { case (args) => (lhs: Ast.expr) => Ast.expr.Call(lhs, args)}
+    val slice = P("[" ~ test ~ "]").map { case (args) => (lhs: Ast.expr) => Ast.expr.Subscript(lhs, args)}
     val cast = P( "." ~ "as" ~ "<" ~ TYPE_NAME ~ ">" ).map(
       typeName => (lhs: Ast.expr) => Ast.expr.CastToType(lhs, typeName)
     )
@@ -145,16 +145,16 @@ object Expressions {
 
   val exprlist: P[Seq[Ast.expr]] = P( expr.rep(1, sep = ",") ~ ",".? )
   val testlist: P[Seq[Ast.expr]] = P( test.rep(1, sep = ",") ~ ",".? )
-//  val dictorsetmaker: P[Ast.expr] = {
-//    val dict_item = P( test ~ ":" ~ test )
-//    val dict: P[Ast.expr.Dict] = P(
-//      (dict_item.rep(1, ",") ~ ",".?).map{x =>
-//        val (keys, values) = x.unzip
-//        Ast.expr.Dict(keys, values)
-//      }
-//    )
-//    P( /*dict_comp |*/ dict /*| set_comp | set*/)
-//  }
+  // val dictorsetmaker: P[Ast.expr] = {
+  //   val dict_item = P( test ~ ":" ~ test )
+  //   val dict: P[Ast.expr.Dict] = P(
+  //     (dict_item.rep(1, ",") ~ ",".?).map { x =>
+  //       val (keys, values) = x.unzip
+  //       Ast.expr.Dict(keys, values)
+  //     }
+  //   )
+  //   P( /*dict_comp |*/ dict /*| set_comp | set*/)
+  // }
 
   val arglist: P[Seq[Ast.expr]] = P( (test).rep(0, ",") )
 
