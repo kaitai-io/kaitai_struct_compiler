kaitai-io/kaitai_struct_compiler

commit eb420a617ae91a803659dfc29c8a8b54516b7b70

Fix ownership detection of value instance types

shared/src/main/scala/io/kaitai/struct/precompile/ValueTypesDeriver.scala
@@ -24,7 +24,7 @@ class ValueTypesDeriver(specs: ClassSpecs, topClass: ClassSpec) {
             vi.dataType match {
               case None =>
                 try {
-                  val viType = detector.detectType(vi.value).asNonOwning
+                  val viType = detector.detectType(vi.value)
                   vi.dataType = Some(viType)
                   Log.typeProcValue.info(() => s"${instName.name} derived type: $viType")
                   hasChanged = true

shared/src/main/scala/io/kaitai/struct/translators/TypeDetector.scala
@@ -57,7 +57,7 @@ class TypeDetector(provider: TypeProvider) {
         val t = EnumType(List(enumType.name), CalcIntType)
         t.enumSpec = Some(provider.resolveEnum(inType, enumType.name))
         t
-      case Ast.expr.Name(name: Ast.identifier) => provider.determineType(name.name)
+      case Ast.expr.Name(name: Ast.identifier) => provider.determineType(name.name).asNonOwning
       case Ast.expr.UnaryOp(op: Ast.unaryop, v: Ast.expr) =>
         val t = detectType(v)
         (t, op) match {
@@ -99,7 +99,7 @@ class TypeDetector(provider: TypeProvider) {
           case other => throw new TypeMismatchError(s"unable to switch over $other")
         }
       case Ast.expr.Subscript(container: Ast.expr, idx: Ast.expr) =>
-        detectType(container) match {
+        (detectType(container) match {
           case ArrayTypeInStream(elType: DataType) =>
             detectType(idx) match {
               case _: IntType => elType
@@ -112,11 +112,11 @@ class TypeDetector(provider: TypeProvider) {
             }
           case _: BytesType => Int1Type(false)
           case cntType => throw new TypeMismatchError(s"unable to apply operation [] to $cntType")
-        }
+        }).asNonOwning
       case Ast.expr.Attribute(value: Ast.expr, attr: Ast.identifier) =>
-        detectAttributeType(value, attr)
+        detectAttributeType(value, attr).asNonOwning
       case call: Ast.expr.Call =>
-        detectCallType(call)
+        detectCallType(call).asNonOwning
       case Ast.expr.List(values: Seq[Ast.expr]) =>
         detectArrayType(values) match {
           case Int1Type(_) => CalcBytesType

commit f9d9fd7433b186a72488c70f8b9326a1a260f00a

Expect borrowed user type in variable and attribute access
Fixes tests that were broken after eb420a617ae91a803659dfc29c8a8b54516b7b70

jvm/src/test/scala/io/kaitai/struct/translators/TranslatorSpec.scala
@@ -151,7 +151,7 @@ class TranslatorSpec extends FunSuite {
     RubyCompiler -> "foo_str"
   ))
 
-  full("foo_block", userOwnedType(List("block")), userOwnedType(List("block")), Map[LanguageCompilerStatic, String](
+  full("foo_block", userOwnedType(List("block")), userBorrowedType(List("block")), Map[LanguageCompilerStatic, String](
     CppCompiler -> "foo_block()",
     CSharpCompiler -> "FooBlock",
     GoCompiler -> "this.FooBlock",
@@ -190,7 +190,7 @@ class TranslatorSpec extends FunSuite {
     RubyCompiler -> "foo.inner.baz"
   ))
 
-  full("_root.foo", userOwnedType(List("top_class", "block")), userOwnedType(List("top_class", "block")), Map[LanguageCompilerStatic, String](
+  full("_root.foo", userOwnedType(List("top_class", "block")), userBorrowedType(List("top_class", "block")), Map[LanguageCompilerStatic, String](
     CppCompiler -> "_root()->foo()",
     CSharpCompiler -> "M_Root.Foo",
     GoCompiler -> "this._root.Foo",
@@ -737,6 +737,13 @@ class TranslatorSpec extends FunSuite {
   def userOwnedType(lname: List[String]) = {
     val cs = ClassSpec.opaquePlaceholder(lname)
     val ut = UserTypeInstream(lname, None)
+    ut.classSpec = Some(cs)
+    ut
+  }
+
+  def userBorrowedType(lname: List[String]) = {
+    val cs = ClassSpec.opaquePlaceholder(lname)
+    val ut = CalcUserType(lname, None)
     ut.classSpec = Some(cs)
     ut
   }