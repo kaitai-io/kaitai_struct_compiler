1cbaff9be2a1fd6b9b2196e46be7c96ac4f87290
Petr Pucil <petr.pucil@seznam.cz>
2022-06-26 09:00:33 +0200

    Prohibit duplicate enum member IDs (names)
    Silently allowing duplicate enum member IDs causes problems in real
    formats, see
    - https://github.com/kaitai-io/kaitai_struct_formats/issues/345
    - https://github.com/kaitai-io/kaitai_struct_formats/issues/346
    - https://github.com/kaitai-io/kaitai_struct_formats/pull/421#discussion_r588939814
    - https://github.com/kaitai-io/kaitai_struct_formats/pull/448

--- shared/src/main/scala/io/kaitai/struct/format/EnumSpec.scala
+++ shared/src/main/scala/io/kaitai/struct/format/EnumSpec.scala
@@ -1,5 +1,9 @@
 package io.kaitai.struct.format
 
+import io.kaitai.struct.problems.KSYParseError
+
+import scala.collection.mutable
+
 case class EnumSpec(map: Map[Long, EnumValueSpec]) {
   var name = List[String]()
 
@@ -19,10 +23,18 @@ case class EnumSpec(map: Map[Long, EnumValueSpec]) {
 object EnumSpec {
   def fromYaml(src: Any, path: List[String]): EnumSpec = {
     val srcMap = ParseUtils.asMap(src, path)
+    val memberNameMap = mutable.Map[String, Long]()
     EnumSpec(srcMap.map { case (id, desc) =>
       val idLong = ParseUtils.asLong(id, path)
       val value = EnumValueSpec.fromYaml(desc, path ++ List(idLong.toString))
 
+      memberNameMap.get(value.name).foreach { (prevIdLong) =>
+        throw KSYParseError.withText(
+          s"duplicate enum member ID: '${value.name}', previously defined at /${(path ++ List(prevIdLong.toString)).mkString("/")}",
+          path ++ List(idLong.toString)
+        )
+      }
+      memberNameMap.put(value.name, idLong)
       idLong -> value
     })
   }
