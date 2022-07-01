74759af6b58781a9171aae54409bfd34296ed827
Petr Pucil <petr.pucil@seznam.cz>
2022-07-01 09:32:23 +0200

    Fix --ksc-json-output crash (scala.MatchError on ArrayBuffer)

--- shared/src/main/scala/io/kaitai/struct/JSON.scala
+++ shared/src/main/scala/io/kaitai/struct/JSON.scala
@@ -22,7 +22,7 @@ object JSON extends CommonLiterals {
       case v: Jsonable => v.toJson
       case v: Int => v.toString
       case v: String => stringToJson(v)
-      case v: List[_] => listToJson(v)
+      case v: Seq[_] => seqToJson(v)
       case v: Map[String, _] => mapToJson(v)
     }
   }
@@ -30,7 +30,7 @@ object JSON extends CommonLiterals {
   def stringToJson(str: String): String =
     doStringLiteral(str)
 
-  def listToJson(obj: List[_]): String =
+  def seqToJson(obj: Seq[_]): String =
     "[" + obj.map((x) => stringify(x)).mkString(",") + "]"
 
   def mapToJson(obj: Map[String, _]): String = {
