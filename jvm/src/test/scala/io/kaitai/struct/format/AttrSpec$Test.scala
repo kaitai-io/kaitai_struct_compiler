4bd808a4df0fc9ef2b6d789a9bd7050ace685685
Petr Pucil <petr.pucil@seznam.cz>
2022-07-01 22:27:11 +0200

    AttrSpec$Test: fix expected error messages after 977df8fa

--- jvm/src/test/scala/io/kaitai/struct/format/AttrSpec$Test.scala
+++ jvm/src/test/scala/io/kaitai/struct/format/AttrSpec$Test.scala
@@ -56,17 +56,23 @@ class AttrSpec$Test extends FunSpec {
 
     it ("fails to parse double") {
       the [CompilationProblemException] thrownBy tryOne("1.234") should
-        have message("(main): /test: error: unable to parse fixed content: 1.234")
+        have message("""(main): /test:
+                       |	error: unable to parse fixed content: 1.234
+                       |""".stripMargin)
     }
 
     it ("fails to parse map") {
       the [CompilationProblemException] thrownBy tryOne("foo: 123") should
-        have message("(main): /test: error: unable to parse fixed content: Map(foo -> 123)")
+        have message("""(main): /test:
+                       |	error: unable to parse fixed content: Map(foo -> 123)
+                       |""".stripMargin)
     }
 
     it ("fails to parse bogus array element") {
       the [CompilationProblemException] thrownBy tryOne("[1, 2, [3]]") should
-        have message("(main): /test/2: error: unable to parse fixed content in array: List(3)")
+        have message("""(main): /test/2:
+                       |	error: unable to parse fixed content in array: List(3)
+                       |""".stripMargin)
     }
   }
 
