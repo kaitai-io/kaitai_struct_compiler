a477800c4eba850a00302629289f1ac04f0844d1
Petr Pucil <petr.pucil@seznam.cz>
2023-08-05 20:53:31 +0200

    PythonCompiler: fix "{alignToByte => align_to_byte}()" in comment

--- shared/src/main/scala/io/kaitai/struct/languages/PythonCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/PythonCompiler.scala
@@ -483,8 +483,8 @@ class PythonCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
   override def popPos(io: String): Unit =
     out.puts(s"$io.seek(_pos)")
 
-  // NOTE: the compiler does not need to output alignToByte() calls for Python anymore, since the byte
-  // alignment is handled by the runtime library since commit
+  // NOTE: the compiler does not need to output align_to_byte() calls for Python anymore,
+  // since the byte alignment is handled by the runtime library since commit
   // https://github.com/kaitai-io/kaitai_struct_python_runtime/commit/1cb84b84d358e1cdffe35845d1e6688bff923952
   override def alignToByte(io: String): Unit = {}
 
