abc3221fafefca50460487c3e5273a063e4a7ec2
Dias Rystin <drystin@azul.com>
2023-08-08 14:39:45 +0200

    JuliaTranslator can handle basic expressions

--- shared/src/main/scala/io/kaitai/struct/translators/JuliaTranslator.scala
+++ shared/src/main/scala/io/kaitai/struct/translators/JuliaTranslator.scala
@@ -7,17 +7,32 @@ import io.kaitai.struct.format.Identifier
 import io.kaitai.struct.languages.{JuliaCompiler, RubyCompiler}
 
 class JuliaTranslator(provider: TypeProvider, importList: ImportList) extends BaseTranslator(provider) {
-  override def numericBinOp(left: Ast.expr, op: Ast.operator, right: Ast.expr) = {
+  override def numericBinOp(left: Ast.expr, op: Ast.operator, right: Ast.expr): String = {
     (detectType(left), detectType(right), op) match {
       case (_: IntType, _: IntType, Ast.operator.Div) =>
-        s"${translate(left)} // ${translate(right)}"
+        s"${translate(left)} ÷ ${translate(right)}"
       case _ =>
         super.numericBinOp(left, op, right)
     }
   }
 
-  override def doStringLiteral(s: String): String = "u" + super.doStringLiteral(s)
-  override def doBoolLiteral(n: Boolean): String = if (n) "True" else "False"
+  override def binOp(op: Ast.operator): String = {
+    op match {
+      case Ast.operator.Add => "+"
+      case Ast.operator.Sub => "-"
+      case Ast.operator.Mult => "*"
+      case Ast.operator.Div => "/"
+      case Ast.operator.Mod => "%"
+      case Ast.operator.BitAnd => "&"
+      case Ast.operator.BitOr => "|"
+      case Ast.operator.BitXor => "⊻"
+      case Ast.operator.LShift => "<<"
+      case Ast.operator.RShift => ">>"
+    }
+  }
+
+//  override def doStringLiteral(s: String): String = super.doStringLiteral(s)
+//  override def doBoolLiteral(n: Boolean): String = if (n) "True" else "False"
 
   /**
     * https://docs.Julia.org/2.7/reference/lexical_analysis.html#string-literals
@@ -37,43 +52,39 @@ class JuliaTranslator(provider: TypeProvider, importList: ImportList) extends Ba
   )
 
   override def doByteArrayLiteral(arr: Seq[Byte]): String =
-    "b\"" + Utils.hexEscapeByteArray(arr) + "\""
+    s"Vector{UInt8}(${super.doByteArrayLiteral(arr)})"
   override def doByteArrayNonLiteral(elts: Seq[Ast.expr]): String = {
-    importList.add("import struct")
-    s"struct.pack('${elts.length}b', ${elts.map(translate).mkString(", ")})"
+//    importList.add("import struct")
+//    s"struct.pack('${elts.length}b', ${elts.map(translate).mkString(", ")})"
+    s"Vector{UInt8}([${elts.map(translate).mkString(", ")}])"
   }
 
-  override def doLocalName(s: String) = {
+  override def doLocalName(s: String): String = {
     s match {
-      case Identifier.ITERATOR => "_"
+      case Identifier.ITERATOR => "_it"
       case Identifier.INDEX => "i"
-      case _ => s"self.${doName(s)}"
+      case _ => s"this.${doName(s)}"
     }
   }
-  override def doName(s: String) =
-    s
+  override def doName(s: String): String = {
+    s match {
+      case Identifier.ITERATOR => "_it"
+      case _ => s
+    }
+  }
+
   override def doInternalName(id: Identifier): String =
-    s"self.${JuliaCompiler.publicMemberName(id)}"
+    s"this.${JuliaCompiler.publicMemberName(id)}"
 
   override def doEnumByLabel(enumTypeAbs: List[String], label: String): String =
-    s"${JuliaCompiler.types2class(enumTypeAbs)}.$label"
+    s"${JuliaCompiler.types2class(enumTypeAbs)}_$label"
   override def doEnumById(enumTypeAbs: List[String], id: String): String =
-    s"${JuliaCompiler.kstreamName}.resolve_enum(${JuliaCompiler.types2class(enumTypeAbs)}, $id)"
-
-  override def booleanOp(op: Ast.boolop) = op match {
-    case Ast.boolop.Or => "or"
-    case Ast.boolop.And => "and"
-  }
-
-  override def unaryOp(op: Ast.unaryop) = op match {
-    case Ast.unaryop.Not => "not "
-    case _ => super.unaryOp(op)
-  }
+    s"${JuliaCompiler.types2class(enumTypeAbs)}($id)"
 
   override def arraySubscript(container: Ast.expr, idx: Ast.expr): String =
     s"${translate(container)}[${translate(idx)}]"
   override def doIfExp(condition: Ast.expr, ifTrue: Ast.expr, ifFalse: Ast.expr): String =
-    s"(${translate(ifTrue)} if ${translate(condition)} else ${translate(ifFalse)})"
+    s"(${translate(condition)} ? ${translate(ifTrue)} : ${translate(ifFalse)})"
 
   // Predefined methods of various types
   override def strToInt(s: Ast.expr, base: Ast.expr): String = {
@@ -92,21 +103,17 @@ class JuliaTranslator(provider: TypeProvider, importList: ImportList) extends Ba
     s"int(${translate(v)})"
   override def intToStr(i: Ast.expr, base: Ast.expr): String = {
     val baseStr = translate(base)
-    val func = baseStr match {
-      case "2" => "bin"
-      case "8" => "oct"
-      case "10" => "str"
-      case "16" => "hex"
-      case _ => throw new UnsupportedOperationException(baseStr)
-    }
-
-    s"$func(${translate(i)})"
+    s"string(${translate(i)}, base = $baseStr)"
   }
-  override def bytesToStr(bytesExpr: String, encoding: Ast.expr): String =
-    s"($bytesExpr).decode(${translate(encoding)})"
+  override def bytesToStr(bytesExpr: String, encoding: Ast.expr): String = {
+    importList.add("using StringEncodings")
+    s"decode(($bytesExpr), ${translate(encoding)})"
+  }
+
+  override def strConcat(left: Ast.expr, right: Ast.expr): String = s"${translate(left)} * ${translate(right)}"
 
   override def bytesLength(value: Ast.expr): String =
-    s"len(${translate(value)})"
+    s"length(${translate(value)})"
   override def bytesSubscript(container: Ast.expr, idx: Ast.expr): String =
     s"${JuliaCompiler.kstreamName}.byte_array_index(${translate(container)}, ${translate(idx)})"
   override def bytesFirst(a: Ast.expr): String =
@@ -120,22 +127,22 @@ class JuliaTranslator(provider: TypeProvider, importList: ImportList) extends Ba
 
 
   override def strLength(value: Ast.expr): String =
-    s"len(${translate(value)})"
+    s"length(${translate(value)})"
   override def strReverse(value: Ast.expr): String =
-    s"(${translate(value)})[::-1]"
+    s"reverse(${translate(value)})"
   override def strSubstring(s: Ast.expr, from: Ast.expr, to: Ast.expr): String =
     s"(${translate(s)})[${translate(from)}:${translate(to)}]"
 
   override def arrayFirst(a: Ast.expr): String =
-    s"${translate(a)}[0]"
+    s"${translate(a)}[1]"
   override def arrayLast(a: Ast.expr): String =
-    s"${translate(a)}[-1]"
+    s"${translate(a)}[end]"
   override def arraySize(a: Ast.expr): String =
-    s"len(${translate(a)})"
+    s"size(${translate(a)}, 1)"
   override def arrayMin(a: Ast.expr): String =
-    s"min(${translate(a)})"
+    s"minimum(${translate(a)})"
   override def arrayMax(a: Ast.expr): String =
-    s"max(${translate(a)})"
+    s"maximum(${translate(a)})"
 
   override def kaitaiStreamSize(value: Ast.expr): String =
     s"${translate(value)}.size()"
