bd2784696cb1d685bafb9f159c500b3e3fadc5a2
Mingun <Alexander_Sergey@mail.ru>
2024-03-15 20:25:38 +0500

    Document CalculateFullNamesAndSetSurroundingType pass and improve documentation of related ClassSpec and EnumSpec properties

--- shared/src/main/scala/io/kaitai/struct/precompile/CalculateFullNamesAndSetSurroundingType.scala
+++ shared/src/main/scala/io/kaitai/struct/precompile/CalculateFullNamesAndSetSurroundingType.scala
@@ -3,6 +3,10 @@ package io.kaitai.struct.precompile
 import io.kaitai.struct.format.{ClassSpec, ClassSpecs}
 import io.kaitai.struct.problems.CompilationProblem
 
+/**
+  * Assigns a full name to the `.name` property to each KS type and enum.
+  * Also for each [[ClassSpec]] assigns [[ClassSpec.upClass]] to a surrounding type.
+  */
 class CalculateFullNamesAndSetSurroundingType(classSpecs: ClassSpecs) extends PrecompileStep {
   override def run(): Iterable[CompilationProblem] = {
     classSpecs.foreach { case (_, curClass) => calculate(curClass) }
