kaitai-io/kaitai_struct_compiler

commit 7154301a257423e5d68b200730be1c029e660433

Ensure that `contents` and `valid` apply to individual elements
Resolves https://github.com/kaitai-io/kaitai_struct/issues/1117

This is how the `contents` key worked until KS 0.8, but then in KS 0.9,
`contents` started to be converted to the newly introduced `valid`,
which was applied only once at the end on the final value. This means
that it was applied on the entire array in the case of repeated fields.
However, the behavior of `valid` + `repeat` wasn't tested anywhere, so I
think it was an oversight. Note that the Kaitai Struct language doesn't
define an equality `==` operation for true arrays, so it wouldn't even
work. Running the `valid` and `contents` checks on each element is more
flexible (and also seems more useful).

Fixes the following tests for all languages except Go (will be fixed in
the following commit), Nim, Perl and Construct:

* ValidFailRepeatAnyofInt
* ValidFailRepeatContents
* ValidFailRepeatEqInt
* ValidFailRepeatExpr
* ValidFailRepeatInst
* ValidFailRepeatMaxInt
* ValidFailRepeatMinInt

These tests were added in
https://github.com/kaitai-io/kaitai_struct_tests/commit/98a68fcf607a9f8d4fb5febb400a528df388dea2

shared/src/main/scala/io/kaitai/struct/languages/components/CommonReads.scala
@@ -60,9 +60,6 @@ trait CommonReads extends LanguageCompiler {
       case _ => // no seeking required for sequence attributes
     }
 
-    // Run validations (still inside "if", if applicable)
-    attrValidateAll(attr)
-
     attrParseIfFooter(attr.cond.ifExpr)
   }
 
@@ -79,6 +76,7 @@ trait CommonReads extends LanguageCompiler {
       case NoRepeat =>
     }
     attrParse2(id, attr.dataType, io, attr.cond.repeat, false, defEndian)
+    attrValidateAll(attr)
     attr.cond.repeat match {
       case RepeatEos =>
         condRepeatEosFooter

shared/src/main/scala/io/kaitai/struct/format/Identifier.scala
@@ -70,6 +70,19 @@ object Identifier {
     }
   }
 
+  def itemExpr(id: Identifier, rep: RepeatSpec): Ast.expr = {
+    val astId = Ast.expr.InternalName(id)
+    rep match {
+      case NoRepeat =>
+        astId
+      case _ =>
+        Ast.expr.Subscript(
+          astId,
+          Ast.expr.Name(Ast.identifier(Identifier.INDEX))
+        )
+    }
+  }
+
   // Constants for special names used in expression language
   val ROOT = "_root"
   val PARENT = "_parent"

shared/src/main/scala/io/kaitai/struct/languages/components/ValidateOps.scala
@@ -14,22 +14,23 @@ trait ValidateOps extends ExceptionNames {
   val typeProvider: ClassTypeProvider
 
   def attrValidate(attrId: Identifier, attr: AttrLikeSpec, valid: ValidationSpec): Unit = {
+    val itemValue = Identifier.itemExpr(attrId, attr.cond.repeat)
     valid match {
       case ValidationEq(expected) =>
-        attrValidateExprCompare(attrId, attr, Ast.cmpop.Eq, expected, ValidationNotEqualError(attr.dataTypeComposite))
+        attrValidateExprCompare(attrId, attr, Ast.cmpop.Eq, expected, ValidationNotEqualError(attr.dataType))
       case ValidationMin(min) =>
-        attrValidateExprCompare(attrId, attr, Ast.cmpop.GtE, min, ValidationLessThanError(attr.dataTypeComposite))
+        attrValidateExprCompare(attrId, attr, Ast.cmpop.GtE, min, ValidationLessThanError(attr.dataType))
       case ValidationMax(max) =>
-        attrValidateExprCompare(attrId, attr, Ast.cmpop.LtE, max, ValidationGreaterThanError(attr.dataTypeComposite))
+        attrValidateExprCompare(attrId, attr, Ast.cmpop.LtE, max, ValidationGreaterThanError(attr.dataType))
       case ValidationRange(min, max) =>
-        attrValidateExprCompare(attrId, attr, Ast.cmpop.GtE, min, ValidationLessThanError(attr.dataTypeComposite))
-        attrValidateExprCompare(attrId, attr, Ast.cmpop.LtE, max, ValidationGreaterThanError(attr.dataTypeComposite))
+        attrValidateExprCompare(attrId, attr, Ast.cmpop.GtE, min, ValidationLessThanError(attr.dataType))
+        attrValidateExprCompare(attrId, attr, Ast.cmpop.LtE, max, ValidationGreaterThanError(attr.dataType))
       case ValidationAnyOf(values) =>
         val bigOrExpr = Ast.expr.BoolOp(
           Ast.boolop.Or,
           values.map(expected =>
             Ast.expr.Compare(
-              Ast.expr.InternalName(attrId),
+              itemValue,
               Ast.cmpop.Eq,
               expected
             )
@@ -38,30 +39,30 @@ trait ValidateOps extends ExceptionNames {
 
         attrValidateExpr(
           attrId,
-          attr.dataTypeComposite,
+          attr.dataType,
           checkExpr = bigOrExpr,
-          err = ValidationNotAnyOfError(attr.dataTypeComposite),
+          err = ValidationNotAnyOfError(attr.dataType),
           errArgs = List(
-            Ast.expr.InternalName(attrId),
+            itemValue,
             Ast.expr.InternalName(IoIdentifier),
             Ast.expr.Str(attr.path.mkString("/", "/", ""))
           )
         )
       case ValidationExpr(expr) =>
         blockScopeHeader
-        typeProvider._currentIteratorType = Some(attr.dataTypeComposite)
+        typeProvider._currentIteratorType = Some(attr.dataType)
         handleAssignmentTempVar(
-          attr.dataTypeComposite,
+          attr.dataType,
           translator.translate(Ast.expr.Name(Ast.identifier(Identifier.ITERATOR))),
-          translator.translate(Ast.expr.InternalName(attrId))
+          translator.translate(itemValue)
         )
         attrValidateExpr(
           attrId,
-          attr.dataTypeComposite,
+          attr.dataType,
           expr,
-          ValidationExprError(attr.dataTypeComposite),
+          ValidationExprError(attr.dataType),
           List(
-            Ast.expr.InternalName(attrId),
+            itemValue,
             Ast.expr.InternalName(IoIdentifier),
             Ast.expr.Str(attr.path.mkString("/", "/", ""))
           )
@@ -71,18 +72,19 @@ trait ValidateOps extends ExceptionNames {
   }
 
   def attrValidateExprCompare(attrId: Identifier, attr: AttrLikeSpec, op: Ast.cmpop, expected: Ast.expr, err: KSError): Unit = {
+    val itemValue = Identifier.itemExpr(attrId, attr.cond.repeat)
     attrValidateExpr(
       attrId,
-      attr.dataTypeComposite,
+      attr.dataType,
       checkExpr = Ast.expr.Compare(
-        Ast.expr.InternalName(attrId),
+        itemValue,
         op,
         expected
       ),
       err = err,
       errArgs = List(
         expected,
-        Ast.expr.InternalName(attrId),
+        itemValue,
         Ast.expr.InternalName(IoIdentifier),
         Ast.expr.Str(attr.path.mkString("/", "/", ""))
       )

commit 604866249cab73c51838ffedb2c672073dd55e7a

Write end offsets to `_debug` even if validation fails
Fixes a regression introduced in
7154301a257423e5d68b200730be1c029e660433 that was well noticeable in the
devel Web IDE with the
https://github.com/kaitai-io/kaitai_struct_webide/issues/156 feature: a
field with a failed validation (specified by the `contents` or `valid`
key) no longer had its byte range highlighted in the hex dump. This is
because the aforementioned commit changed the order of operations in the
generated JavaScript code (in debug mode, or more precisely with
`--read-pos`) so that the assignment of the end offset to the `_debug`
map for a field with `contents` or `valid` was done *after* the `if`
statement performing the validation, which meant that the end offset was
not assigned if the validation failed.

A nice side effect of this commit is that it eliminates duplicate
`this._debug.{ATTR_NAME}.arr[i]` assignments in the case of a field with
type switching and `repeat`, such as when you compile
https://github.com/kaitai-io/kaitai_struct_tests/blob/9f782819b70db6069674995b969d2b801c86a515/formats/switch_repeat_expr.ksy
in `--read-pos` mode (in JavaScript, it removes the
`this._debug.body.arr[i] = ...` assignments from every branch of the
`switch` statement, which are indeed unnecessary, because the exact same
assignments already exist around the `switch` statement as well).

Note: this commit unfortunately doesn't have any impact on the current
set of test formats in
[`formats/`](https://github.com/kaitai-io/kaitai_struct_tests/tree/9f782819b70db6069674995b969d2b801c86a515/formats)
when compiled with the usual settings in
[`build-formats`](https://github.com/kaitai-io/kaitai_struct_tests/blob/9f782819b70db6069674995b969d2b801c86a515/build-formats).
This is because we only test the debug mode on 4 formats that have the
`meta/ks-debug: true` key, and none of them has a field with `contents`
or `valid`. But if you add `--debug` to the KSC incantation in
`build-formats`, you will see the changes.

shared/src/main/scala/io/kaitai/struct/languages/components/CommonReads.scala
@@ -25,12 +25,11 @@ trait CommonReads extends LanguageCompiler {
         normalIO
     }
 
-    val needsDebug = attrDebugNeeded(id)
+    val needsArrayDebug = attrDebugNeeded(id) && attr.cond.repeat != NoRepeat
 
-    if (needsDebug) {
+    if (needsArrayDebug) {
       attrDebugStart(id, attr.dataType, Some(io), NoRepeat)
-      if (attr.cond.repeat != NoRepeat)
-        attrDebugArrInit(id, attr.dataType)
+      attrDebugArrInit(id, attr.dataType)
     }
 
     defEndian match {
@@ -45,7 +44,7 @@ trait CommonReads extends LanguageCompiler {
         attrParse0(id, attr, io, Some(fe))
     }
 
-    if (needsDebug)
+    if (needsArrayDebug)
       attrDebugEnd(id, attr.dataType, io, NoRepeat)
 
     // More position management + set calculated flag after parsing for ParseInstanceSpecs
@@ -75,7 +74,16 @@ trait CommonReads extends LanguageCompiler {
         condRepeatUntilHeader(id, io, attr.dataType, untilExpr)
       case NoRepeat =>
     }
+
+    val needsDebug = attrDebugNeeded(id)
+    if (needsDebug)
+      attrDebugStart(id, attr.dataType, Some(io), attr.cond.repeat)
+
     attrParse2(id, attr.dataType, io, attr.cond.repeat, false, defEndian)
+
+    if (needsDebug)
+      attrDebugEnd(id, attr.dataType, io, attr.cond.repeat)
+
     attrValidateAll(attr)
     attr.cond.repeat match {
       case RepeatEos =>
@@ -94,7 +102,15 @@ trait CommonReads extends LanguageCompiler {
   def attrDebugArrInit(attrId: Identifier, attrType: DataType): Unit = {}
   def attrDebugEnd(attrName: Identifier, attrType: DataType, io: String, repeat: RepeatSpec): Unit = {}
 
-  def attrDebugNeeded(attrId: Identifier): Boolean = false
+  def attrDebugNeeded(attrId: Identifier): Boolean = {
+    if (!config.readStoresPos)
+      return false
+
+    attrId match {
+      case _: NamedIdentifier | _: NumberedIdentifier | _: InstanceIdentifier => true
+      case _ => false
+    }
+  }
 
   /**
     * Runs all validation procedures requested for an attribute.

shared/src/main/scala/io/kaitai/struct/languages/components/EveryReadIsExpression.scala
@@ -31,10 +31,6 @@ trait EveryReadIsExpression
     assignTypeOpt: Option[DataType] = None
   ): Unit = {
     val assignType = assignTypeOpt.getOrElse(dataType)
-    val needsDebug = attrDebugNeeded(id) && rep != NoRepeat
-
-    if (needsDebug)
-      attrDebugStart(id, dataType, Some(io), rep)
 
     dataType match {
       case t: UserType =>
@@ -61,9 +57,6 @@ trait EveryReadIsExpression
         val expr = parseExpr(dataType, assignType, io, defEndian)
         handleAssignment(id, expr, rep, isRaw)
     }
-
-    if (needsDebug)
-      attrDebugEnd(id, dataType, io, rep)
   }
 
   def attrBytesTypeParse(
@@ -254,15 +247,5 @@ trait EveryReadIsExpression
     if (attrDebugNeeded(instName))
       attrDebugStart(instName, dataType, None, NoRepeat)
     handleAssignmentSimple(instName, expression(value))
-  }
-
-  override def attrDebugNeeded(attrId: Identifier): Boolean = {
-    if (!config.readStoresPos)
-      return false
-
-    attrId match {
-      case _: NamedIdentifier | _: NumberedIdentifier | _: InstanceIdentifier => true
-      case _ => super.attrDebugNeeded(attrId)
-    }
   }
 }