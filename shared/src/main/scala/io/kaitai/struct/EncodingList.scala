98801eaa78d67a44ed2432dfe0a6d7747b195b7f
Petr Pucil <petr.pucil@seznam.cz>
2024-08-30 12:07:00 +0200

    EncodingList: define `UTF-xxBE` before `UTF-xxLE`

--- shared/src/main/scala/io/kaitai/struct/EncodingList.scala
+++ shared/src/main/scala/io/kaitai/struct/EncodingList.scala
@@ -16,10 +16,10 @@ object EncodingList {
   val canonicalToAlias = Map(
     "ASCII" -> Set("US-ASCII"),
     "UTF-8" -> Set("UTF8", "ISO-10646/UTF-8", "ISO-10646/UTF8"),
-    "UTF-16LE" -> Set("UTF16LE"),
     "UTF-16BE" -> Set("UTF16BE"),
-    "UTF-32LE" -> Set("UTF32LE"),
+    "UTF-16LE" -> Set("UTF16LE"),
     "UTF-32BE" -> Set("UTF32BE"),
+    "UTF-32LE" -> Set("UTF32LE"),
     "ISO-8859-1" -> Set("ISO8859-1", "ISO_8859-1", "ISO88591", "8859_1", "8859-1", "88591", "LATIN1", "IBM819", "cp819", "csISOLatin1", "iso-ir-100", "windows-28591", "WE8ISO8859P1"),
     "ISO-8859-2" -> Set("ISO8859-2", "ISO_8859-2", "ISO88592", "8859_2", "8859-2", "88592", "LATIN2", "IBM1111", "cp1111", "csISOLatin2", "iso-ir-101", "windows-28592"),
     "ISO-8859-3" -> Set("ISO8859-3", "ISO_8859-3", "ISO88593", "8859_3", "8859-3", "88593", "LATIN3", "csISOLatin3", "iso-ir-109", "windows-28593"),
