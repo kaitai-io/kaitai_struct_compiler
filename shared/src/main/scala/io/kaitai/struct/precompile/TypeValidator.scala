0a2c4a1327fa12d049554baeeff63266fe7e1251
Mingun <Alexander_Sergey@mail.ru>
2024-09-15 19:51:55 +0500

    Set type of `_` for type validation only in `repeat-until` loops where it only can be used
    We do not want that type validation calculate type of actually undefined variable

--- shared/src/main/scala/io/kaitai/struct/precompile/TypeValidator.scala
+++ shared/src/main/scala/io/kaitai/struct/precompile/TypeValidator.scala
@@ -70,12 +70,12 @@ class TypeValidator(specs: ClassSpecs) extends PrecompileStep {
       checkAssert[BooleanType](ifExpr, "boolean", path, "if")
     )
 
-    // Set the type of the `_` variable in expression
-    provider._currentIteratorType = Some(attr.dataType)
     val problemsRepeat: Iterable[CompilationProblem] = attr.cond.repeat match {
       case RepeatExpr(expr) =>
         checkAssert[IntType](expr, "integer", path, "repeat-expr")
       case RepeatUntil(expr) =>
+        // Set the type of the `_` variable in expression
+        provider._currentIteratorType = Some(attr.dataType)
         checkAssert[BooleanType](expr, "boolean", path, "repeat-until")
       case RepeatEos | NoRepeat =>
         None
