d13db325de52a4d62349038f9ab5b76388cf88c4
Mingun <Alexander_Sergey@mail.ru>
2024-10-05 16:46:22 +0500

    Merge ValueTypesDetector into DeriveValueInstanceTypes because it is used only by that pass

--- shared/src/main/scala/io/kaitai/struct/precompile/DeriveValueInstanceTypes.scala
+++ shared/src/main/scala/io/kaitai/struct/precompile/DeriveValueInstanceTypes.scala
@@ -1,7 +1,9 @@
 package io.kaitai.struct.precompile
 
-import io.kaitai.struct.Log
-import io.kaitai.struct.format.ClassSpecs
+import io.kaitai.struct.{ClassTypeProvider, Log}
+import io.kaitai.struct.format.{ClassSpec, ClassSpecs, ValueInstanceSpec}
+import io.kaitai.struct.problems.ErrorInInput
+import io.kaitai.struct.translators.TypeDetector
 
 /**
   * Assign types to value instances by deriving them from their expressions.
@@ -18,12 +20,60 @@ class DeriveValueInstanceTypes(specs: ClassSpecs) {
       Log.typeProcValue.info(() => s"### DeriveValueInstanceTypes: iteration #$iterNum")
       specs.foreach { case (specName, spec) =>
         Log.typeProcValue.info(() => s"#### $specName")
-        val thisChanged = new ValueTypesDeriver(specs, spec).run()
+
+        val provider = new ClassTypeProvider(specs, spec)
+        val detector = new TypeDetector(provider)
+
+        val thisChanged = deriveValueType(spec, provider, detector)
         Log.typeProcValue.info(() => ".... => " + (if (thisChanged) "changed" else "no changes"))
         hasChanged |= thisChanged
       }
       iterNum += 1
     } while (hasChanged)
     Log.typeProcValue.info(() => s"## value type deriving finished in ${iterNum - 1} iteration(s)")
+  }
+
+  private def deriveValueType(
+    curClass: ClassSpec,
+    provider: ClassTypeProvider,
+    detector: TypeDetector
+  ): Boolean = {
+    Log.typeProcValue.info(() => s"deriveValueType(${curClass.nameAsStr})")
+    var hasChanged = false
+
+    provider.nowClass = curClass;
+    curClass.instances.foreach {
+      case (instName, inst) =>
+        inst match {
+          case vi: ValueInstanceSpec =>
+            vi.dataTypeOpt match {
+              case None =>
+                try {
+                  val viType = detector.detectType(vi.value)
+                  vi.dataTypeOpt = Some(viType)
+                  Log.typeProcValue.info(() => s"${instName.name} derived type: $viType")
+                  hasChanged = true
+                } catch {
+                  case tue: TypeUndecidedError =>
+                    Log.typeProcValue.info(() => s"${instName.name} type undecided: ${tue.getMessage}")
+                    // just ignore, we're not there yet, probably we'll get it on next iteration
+                  case err: ExpressionError =>
+                    throw ErrorInInput(err, vi.path ++ List("value")).toException
+                }
+              case Some(_) =>
+                // already derived, do nothing
+            }
+          case _ =>
+            // do nothing
+        }
+    }
+
+    // Continue with all nested types
+    curClass.types.foreach {
+      case (_, classSpec) =>
+        hasChanged ||= deriveValueType(classSpec, provider, detector)
+    }
+
+    hasChanged
   }
 }
