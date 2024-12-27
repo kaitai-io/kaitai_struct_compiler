kaitai-io/kaitai_struct_compiler

commit a5813db003303992af8dae1086c56dc537aa7d66

--no-auto-read: push user object unconditionally after _read()
Resolves https://github.com/kaitai-io/kaitai_struct/issues/1105

See https://github.com/kaitai-io/kaitai_struct/issues/1105#issuecomment-2274496312

This commit changes the order of operations back to "_read() first, then
push", but with push being executed even if _read() fails. It requires
some kind of a `try-finally` block, which is implemented for each target
language (except Nim and Rust, which apparently don't really support
`--no-auto-read`).

shared/src/main/scala/io/kaitai/struct/languages/PerlCompiler.scala
@@ -358,6 +358,26 @@ class PerlCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
   override def userTypeDebugRead(id: String, dataType: DataType, assignType: DataType): Unit =
     out.puts(s"$id->_read();")
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("eval {")
+    out.inc
+    tryBlock()
+    out.puts("1;")
+    out.dec
+    out.puts("} or do {")
+    out.inc
+    out.puts("$failed = 1;")
+    out.puts("$err = $@;")
+    out.dec
+    out.puts("};")
+    finallyBlock()
+    out.puts("if ($failed) {")
+    out.inc
+    out.puts("die $err;")
+    out.dec
+    out.puts("}")
+  }
+
   override def switchStart(id: Identifier, on: Ast.expr): Unit = {}
   override def switchCaseStart(condition: Ast.expr): Unit = {}
   override def switchCaseEnd(): Unit = {}

shared/src/main/scala/io/kaitai/struct/languages/CSharpCompiler.scala
@@ -420,6 +420,21 @@ class CSharpCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     out.puts(s"$expr._read();")
   }
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("try")
+    out.puts("{")
+    out.inc
+    tryBlock()
+    out.dec
+    out.puts("}")
+    out.puts("finally")
+    out.puts("{")
+    out.inc
+    finallyBlock()
+    out.dec
+    out.puts("}")
+  }
+
   override def switchRequiresIfs(onType: DataType): Boolean = onType match {
     case _: IntType | _: EnumType | _: StrType => false
     case _ => true

shared/src/main/scala/io/kaitai/struct/languages/CppCompiler.scala
@@ -749,6 +749,20 @@ class CppCompiler(
     outSrc.puts(s"$expr->_read();")
   }
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    outSrc.puts("try {")
+    outSrc.inc
+    tryBlock()
+    outSrc.dec
+    outSrc.puts("} catch(...) {")
+    outSrc.inc
+    finallyBlock()
+    outSrc.puts("throw;")
+    outSrc.dec
+    outSrc.puts("}")
+    finallyBlock()
+  }
+
   override def switchRequiresIfs(onType: DataType): Boolean = onType match {
     case _: IntType | _: EnumType => false
     case _ => true

shared/src/main/scala/io/kaitai/struct/languages/JavaCompiler.scala
@@ -523,6 +523,18 @@ class JavaCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     out.puts(s"$expr._read();")
   }
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("try {")
+    out.inc
+    tryBlock()
+    out.dec
+    out.puts("} finally {")
+    out.inc
+    finallyBlock()
+    out.dec
+    out.puts("}")
+  }
+
   override def switchCasesRender[T](
     id: Identifier,
     on: Ast.expr,

shared/src/main/scala/io/kaitai/struct/languages/JavaScriptCompiler.scala
@@ -419,6 +419,18 @@ class JavaScriptCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     out.puts(s"$id._read();")
   }
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("try {")
+    out.inc
+    tryBlock()
+    out.dec
+    out.puts("} finally {")
+    out.inc
+    finallyBlock()
+    out.dec
+    out.puts("}")
+  }
+
   override def switchRequiresIfs(onType: DataType): Boolean = onType match {
     case _: IntType | _: BooleanType | _: EnumType | _: StrType => false
     case _ => true

shared/src/main/scala/io/kaitai/struct/languages/LuaCompiler.scala
@@ -376,6 +376,20 @@ class LuaCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
   override def userTypeDebugRead(id: String, dataType: DataType, assignType: DataType): Unit =
     out.puts(s"$id:_read()")
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("local success, err = pcall(function()")
+    out.inc
+    tryBlock()
+    out.dec
+    out.puts("end)")
+    finallyBlock()
+    out.puts("if not success then")
+    out.inc
+    out.puts("error(err)")
+    out.dec
+    out.puts("end")
+  }
+
   override def switchStart(id: Identifier, on: Ast.expr): Unit = {}
   override def switchCaseStart(condition: Ast.expr): Unit = {}
   override def switchCaseEnd(): Unit = {}

shared/src/main/scala/io/kaitai/struct/languages/PHPCompiler.scala
@@ -392,6 +392,18 @@ class PHPCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
   override def userTypeDebugRead(id: String, dataType: DataType, assignType: DataType): Unit =
     out.puts(s"$id->_read();")
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("try {")
+    out.inc
+    tryBlock()
+    out.dec
+    out.puts("} finally {")
+    out.inc
+    finallyBlock()
+    out.dec
+    out.puts("}")
+  }
+
   override def switchStart(id: Identifier, on: Ast.expr): Unit = {
     val onType = translator.detectType(on)
 

shared/src/main/scala/io/kaitai/struct/languages/PythonCompiler.scala
@@ -408,6 +408,17 @@ class PythonCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
   override def userTypeDebugRead(id: String, dataType: DataType, assignType: DataType): Unit =
     out.puts(s"$id._read()")
 
+  override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("try:")
+    out.inc
+    tryBlock()
+    out.dec
+    out.puts("finally:")
+    out.inc
+    finallyBlock()
+    out.dec
+  }
+
   override def switchStart(id: Identifier, on: Ast.expr): Unit = {}
   override def switchCaseStart(condition: Ast.expr): Unit = {}
   override def switchCaseEnd(): Unit = {}

commit 5d3341cb78314712fa399b28ecd7eca8f8fd584b

Perl: fix undeclared variables in tryFinally()
This fixes a bug introduced in a recent commit
a5813db003303992af8dae1086c56dc537aa7d66

shared/src/main/scala/io/kaitai/struct/languages/PerlCompiler.scala
@@ -359,6 +359,7 @@ class PerlCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     out.puts(s"$id->_read();")
 
   override def tryFinally(tryBlock: () => Unit, finallyBlock: () => Unit): Unit = {
+    out.puts("my ($failed, $err);")
     out.puts("eval {")
     out.inc
     tryBlock()